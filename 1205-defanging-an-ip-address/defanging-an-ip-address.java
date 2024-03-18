9f2d62281c96ac50cddeaedb4e101132f4b40a4a
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2024-03-18 09:42:03 +0530

    Time: 1 ms (25.97%) | Memory: 41 MB (95.64%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.java
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.java
@@ -1,31 +1,33 @@
 class Solution {
     public String defangIPaddr(String address) {
-         int n = address.length();
-       StringBuilder sb = new StringBuilder();
-       for(int i=0;i<n;i++){
-           if(address.charAt(i)=='.'){
-               sb.append("[");
-               sb.append(".");
-               sb.append("]");
-           }
-           else{
-               sb.append(address.charAt(i));
-           }
-       } 
-       String str = sb.toString();
-       return str;
-    //    StringBuilder result=new StringBuilder();
-    //    int length=address.length();
-    //    for(int i=0;i<length;i++){
-    //     if(address.charAt(i)=='.'){
-    //          result.append("[.]");
+    //      int n = address.length();
+    //    StringBuilder sb = new StringBuilder();
+    //    for(int i=0;i<n;i++){
+    //        if(address.charAt(i)=='.'){
+    //            sb.append("[");
+    //            sb.append(".");
+    //            sb.append("]");
+    //        }
+    //        else{
+    //            sb.append(address.charAt(i));
+    //        }
+    //    } 
+    //    String str = sb.toString();
+    //    return str;
+       StringBuilder result=new StringBuilder();
+       int length=address.length();
+       for(int i=0;i<length;i++){
+        if(address.charAt(i)=='.'){
+             result.append("[");
+              result.append(".");
+               result.append("]");
            
-    //     }
-    //     else{
-    //         result.append(address.charAt(i));
-    //     }
-    //    }
-    //    System.out.println(result);
-    //    return result.toString();
+        }
+        else{
+            result.append(address.charAt(i));
+        }
+       }
+       System.out.println(result);
+       return result.toString();
     }
 }
