d24fedb61ce343e49d86716890cb3f75643395f0
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2024-05-12 23:32:15 +0530

    Time: 0 ms (100.00%) | Memory: 48.8 MB (59.18%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -5,18 +5,7 @@ class Solution {
     for(int i=0;i<s.length/2;i++){
         temp=s[i];
         s[i]=s[s.length-1-i];
-        s[s.length-1-i]=temp;
-    }
-    //     reverse(s,0,s.length-1);
-    // }
-    // public void reverse(char[] s,int st,int e){
-    //     if(st>=e){
-    //         return;
-    //     }
-    //     char temp=s[st];
-    //     s[st]=s[e];
-    //     s[e]=temp;
-    //     reverse(s,st+1,e-1);
-
+        s[s.length-1-i]=temp;}
+    
     }
 }
