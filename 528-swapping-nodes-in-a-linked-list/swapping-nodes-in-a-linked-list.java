1f3b5d14e9c65a14053f3996c92124d12c460fd0
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2024-06-17 16:34:32 +0530

    Time: 2 ms (60.02%) | Memory: 58.4 MB (35.11%) - LeetSync

---
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode beg=head;
+        ListNode end=head;
+        ListNode last=head;
+        int n=1;
+        while(last.next!=null){
+            if(n>=k){
+                end=end.next;
+            }
+            last=last.next;
+            n++;
+        }
+        int s=1;
+        while(s!=k){
+            beg=beg.next;
+            s++;
+        }
+        int temp=beg.val;
+        beg.val=end.val;
+        end.val=temp;
+        return head;
+    }
+}
