a7039d1cd365b728645443aa29ccf1d2f9d88fa5
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2024-06-19 14:50:42 +0530

    Time: 1 ms (67.84%) | Memory: 40.6 MB (42.68%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -2,14 +2,11 @@ class Solution {
     public boolean isHappy(int n) {
         int s=1,f=0, j=n;;
         while(s!=f){
-           
-            
            s=nextvalue(n);
             n=s;   
               if(s==1){
                 return true;
             }    
-           
             f=nextvalue(nextvalue(j));
              j=f;
                if(f==1){
