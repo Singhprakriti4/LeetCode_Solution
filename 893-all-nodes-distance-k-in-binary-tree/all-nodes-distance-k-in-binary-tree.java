e08703ba4b2fce3728c38e6902ef67f3c04b3d85
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2024-10-15 01:14:34 +0530

    Time: 12 ms (89.44%) | Memory: 42.6 MB (41.16%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -9,27 +9,28 @@
  */
 class Solution {
     List<Integer> ans=new ArrayList<>();
+    Map<TreeNode,TreeNode> m=new HashMap<>();
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         List<Integer> array=new ArrayList<>();
+        parent(root);
         array.add(target.val);
       dist(root,target,target,0,k,array);
       return ans;
     }
-    public TreeNode parent(TreeNode root,TreeNode target){
+    public void parent(TreeNode root){
         if(root==null){
-            return null;
+            return;
         }
-        if(root.left==target || root.right==target){
-            return root;
+        if(root.left!=null){
+            m.put(root.left,root);
         }
-       
-        TreeNode leftparent=  parent(root.left,target);
-        TreeNode rightparent=parent(root.right,target);
-        if(leftparent!=null){
-            return leftparent;
-          }
-        return rightparent;
+        if(root.right!=null){
+            m.put(root.right,root);
+        }
+        parent(root.left);
+        parent(root.right);
     }
+   
     public void dist(TreeNode root,TreeNode target,TreeNode fixed,int curr_count,int req_count,List<Integer> array){
         if(target==null){
             return;
@@ -39,12 +40,16 @@ class Solution {
         return;
        }
 
-       TreeNode p=parent(root,target);
+
+    if(m.containsKey(target)){
+        TreeNode p=m.get(target);
        if( p!=null && p!=fixed && !array.contains(p.val) ){
          array.add(p.val);
        dist(root,p,fixed,curr_count +1, req_count,array);
         array.remove(Integer.valueOf(p.val));
        }
+    }
+       
        if(target.left!=fixed && target.left!=null && !array.contains(target.left.val)){
         array.add(target.left.val);
        dist(root,target.left,fixed,curr_count+1,req_count,array);
