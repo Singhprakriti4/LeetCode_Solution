1883a0051f2124f295774b79887f0b8296198475
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2024-11-07 11:38:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (43.70%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -15,15 +15,22 @@
  */
 class Solution {
     public TreeNode bstFromPreorder(int[] preorder) {
+        //if the preorder is empty i.e. there is no tree
         if(preorder.length==0){
             return null;
         }
+
+        //make the root
        TreeNode root= new TreeNode(preorder[0]);
+
+       //insert all the elements from preorder leaving the 0th index(already created)into the root 
         for(int i=1;i<preorder.length;i++){
            insert(root,preorder[i]) ;
         }
         return root;
     }
+
+    //function to insert a key into a given tree 
   public TreeNode insert(TreeNode root,int key){
     if(root==null){
         return new TreeNode(key);
