17b39a9b2934b1ee08c026e45fe06b3e517255bb
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2024-11-08 23:09:19 +0530

    Time: 3 ms (14.13%) | Memory: 44.7 MB (24.41%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -14,49 +14,41 @@
  * }
  */
 class Solution {
-    List<Integer> list=new ArrayList<>();
+    List<TreeNode> list=new ArrayList<>();
     public void recoverTree(TreeNode root) {
-       inorder(root);
-       int a=Integer.MIN_VALUE,b=Integer.MIN_VALUE;
-       
-       int[] arr=new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            arr[i] = list.get(i);  
+        TreeNode one=null;
+        TreeNode two=null;
+
+        inorder(root);
+        int[] arr=new int[list.size()];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=list.get(i).val;
         }
 
         Arrays.sort(arr);
+
         for(int i=0;i<arr.length;i++){
-            if(arr[i]!=list.get(i)){
-                if(a==Integer.MIN_VALUE)
-                 a=list.get(i);
-                else
-                 b=list.get(i);
+            if(arr[i]!=list.get(i).val){
+                if(one==null){
+                    one=list.get(i);
+                }
+                else{
+                    two=list.get(i);
+                }
             }
         }
-      System.out.println(a+" "+b);
-      reorder(root,a,b);
-    }
-     public void reorder(TreeNode root,int a, int b){
-        if(root==null){
-            return;
-        }
 
-        if(root.val==a){
-            root.val=b;
-        }
-        else if(root.val==b){
-            root.val=a;
-        }
-        reorder(root.left,a,b);
-        reorder(root.right,a,b);
-       }
+        int temp=one.val;
+        one.val=two.val;
+        two.val=temp;
+        
+    }
     public void inorder(TreeNode root){
         if(root==null)
         return;
 
         inorder(root.left);
-        list.add(root.val);
+        list.add(root);
         inorder(root.right);
     }
-
 }
