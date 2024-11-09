ca0d5b29740b10e436b2ad79cb1f57c643a6939c
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2024-11-09 22:10:27 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (48.83%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -14,16 +14,19 @@
  * }
  */
 class Solution {
+    int count=0;
+    int ans=-1;
     public int kthSmallest(TreeNode root, int k) {
-        List<Integer> l=new ArrayList<>();
-        inorder(root,l);
-        return l.get(k-1);
+     inorder(root,k);
+     return ans;
     }
-    public void inorder(TreeNode root,List<Integer> l){
+    public void inorder(TreeNode root,int k){
         if(root==null) return;
 
-        inorder(root.left,l);
-        l.add(root.val);
-        inorder(root.right,l);
+        inorder(root.left,k);
+        count++;
+        if(count==k)
+         ans=root.val;
+        inorder(root.right,k);
     }
 }
