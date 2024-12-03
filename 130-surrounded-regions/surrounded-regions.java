955cb01d7db7bf408bc58852613274f51ea5b854
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2024-12-03 23:39:36 +0530

    Time: 2 ms (84.95%) | Memory: 45.4 MB (42.63%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,22 +1,23 @@
 class Solution {
     public void solve(char[][] board) {
+
         boolean[][] tobechanged=new boolean[board.length][board[0].length];
 
         for(int i=0;i<board.length;i++){
-            if(board[i][0]=='O'){
+            if(board[i][0]=='O' && !tobechanged[i][0]){
             boolean[][] visited1=new boolean[board.length][board[0].length];
             dfs(i,0,visited1,board,tobechanged);}
 
-            if(board[i][board[0].length-1]=='O'){
+            if(board[i][board[0].length-1]=='O' && !tobechanged[i][board[0].length-1]){
              boolean[][] visited2=new boolean[board.length][board[0].length];
             dfs(i,board[0].length-1,visited2,board,tobechanged);}
         }
         for(int i=0;i<board[0].length;i++){
-            if(board[0][i]=='O'){
+            if(board[0][i]=='O' && !tobechanged[0][i]){
              boolean[][] visited3=new boolean[board.length][board[0].length];
             dfs(0,i,visited3,board,tobechanged);}
 
-            if(board[board.length-1][i]=='O'){
+            if(board[board.length-1][i]=='O' && !tobechanged[board.length-1][i]){
              boolean[][] visited4=new boolean[board.length][board[0].length];
             dfs(board.length-1,i,visited4,board,tobechanged);}
         }
