4196b2e7d7f37d1917995ccf46d21009fb14e3d7
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2024-12-03 23:09:37 +0530

    Time: 22 ms (5.21%) | Memory: 45.4 MB (53.54%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.java
@@ -0,0 +1,55 @@
+class Solution {
+    public void solve(char[][] board) {
+        boolean[][] tobechanged=new boolean[board.length][board[0].length];
+
+        for(int i=0;i<board.length;i++){
+            if(board[i][0]=='O'){
+            boolean[][] visited1=new boolean[board.length][board[0].length];
+            dfs(i,0,visited1,board,tobechanged);}
+
+            if(board[i][board[0].length-1]=='O'){
+             boolean[][] visited2=new boolean[board.length][board[0].length];
+            dfs(i,board[0].length-1,visited2,board,tobechanged);}
+        }
+        for(int i=0;i<board[0].length;i++){
+            if(board[0][i]=='O'){
+             boolean[][] visited3=new boolean[board.length][board[0].length];
+            dfs(0,i,visited3,board,tobechanged);}
+
+            if(board[board.length-1][i]=='O'){
+             boolean[][] visited4=new boolean[board.length][board[0].length];
+            dfs(board.length-1,i,visited4,board,tobechanged);}
+        }
+     
+
+     for(int i=0;i<board.length ;i++){
+        for(int j=0;j<board[i].length;j++){
+            if(board[i][j]=='O' && tobechanged[i][j]==false){
+                board[i][j]='X';
+            }
+        }
+     }
+
+    }
+    public void dfs(int row,int col, boolean[][] visited, char[][] board
+    , boolean[][] tobechanged){
+        // if(board[row][col]=='X'){
+        //     return;
+        // }
+        visited[row][col]=true;
+        tobechanged[row][col]=true;
+
+        int[] rowdiff={0,0,-1,1};
+        int[] coldiff={-1,1,0,0};
+
+        for(int i=0;i<4;i++){
+            int r=row+rowdiff[i];
+            int c=col+coldiff[i];
+
+            if(r>=0 && r<board.length && c>=0 && c<board[0].length 
+            && board[r][c]=='O' && !visited[r][c]){
+                dfs(r,c,visited,board,tobechanged);
+            }
+        }
+    }
+}
