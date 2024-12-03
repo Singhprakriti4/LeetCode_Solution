ecadd1342707ca9c874a90b8c0c79f8c42392288
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2024-12-03 23:42:28 +0530

    Time: 2 ms (84.95%) | Memory: 45.8 MB (18.35%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -5,21 +5,17 @@ class Solution {
 
         for(int i=0;i<board.length;i++){
             if(board[i][0]=='O' && !tobechanged[i][0]){
-            boolean[][] visited1=new boolean[board.length][board[0].length];
-            dfs(i,0,visited1,board,tobechanged);}
+            dfs(i,0,board,tobechanged);}
 
             if(board[i][board[0].length-1]=='O' && !tobechanged[i][board[0].length-1]){
-             boolean[][] visited2=new boolean[board.length][board[0].length];
-            dfs(i,board[0].length-1,visited2,board,tobechanged);}
+            dfs(i,board[0].length-1,board,tobechanged);}
         }
         for(int i=0;i<board[0].length;i++){
             if(board[0][i]=='O' && !tobechanged[0][i]){
-             boolean[][] visited3=new boolean[board.length][board[0].length];
-            dfs(0,i,visited3,board,tobechanged);}
+            dfs(0,i,board,tobechanged);}
 
             if(board[board.length-1][i]=='O' && !tobechanged[board.length-1][i]){
-             boolean[][] visited4=new boolean[board.length][board[0].length];
-            dfs(board.length-1,i,visited4,board,tobechanged);}
+            dfs(board.length-1,i,board,tobechanged);}
         }
      
 
@@ -32,12 +28,8 @@ class Solution {
      }
 
     }
-    public void dfs(int row,int col, boolean[][] visited, char[][] board
-    , boolean[][] tobechanged){
-        // if(board[row][col]=='X'){
-        //     return;
-        // }
-        visited[row][col]=true;
+    public void dfs(int row,int col,char[][] board , boolean[][] tobechanged){  
+      
         tobechanged[row][col]=true;
 
         int[] rowdiff={0,0,-1,1};
@@ -48,8 +40,8 @@ class Solution {
             int c=col+coldiff[i];
 
             if(r>=0 && r<board.length && c>=0 && c<board[0].length 
-            && board[r][c]=='O' && !visited[r][c]){
-                dfs(r,c,visited,board,tobechanged);
+            && board[r][c]=='O' && !tobechanged[r][c]){
+                dfs(r,c,board,tobechanged);
             }
         }
     }
