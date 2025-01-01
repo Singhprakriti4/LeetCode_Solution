73eefbfaed470d724ef04b459bcb88e375c89578
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-01-01 22:18:40 +0530

    Time: 1 ms (97.36%) | Memory: 42.1 MB (12.87%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -1,31 +1,27 @@
 class Solution {
-    int maxscore=Integer.MIN_VALUE;
     public int maxScore(String s) {
-        score(s,1,s.substring(0,1),s.substring(1,s.length()));
-      return maxscore;
-    }
-    public void score(String s,int i, String left,String right){
-        if(left.equals("") || right.equals("")){
-            return;
-        }
-        //calculating the score
-        int leftscore=numinstr(left,'0');
-        int rightscore=numinstr(right,'1');
-        int total=leftscore+rightscore;
-
-        maxscore=total>maxscore?total:maxscore;
-
-        score(s,i+1,s.substring(0,i),s.substring(i,s.length()));
-    }
-
-    //calculating number of 0 or 1
-    public int numinstr(String s, char c){
-        int ans=0;
+        int maxscore=0;
+        int ones=0;
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)==c){
-                ans++;
+            if(s.charAt(i)=='1'){
+                ones++;
             }
         }
-        return ans;
+        int zeros=0;
+        int currscore=0;
+        for(int i=0;i<s.length()-1;i++){
+
+           if(s.charAt(i)=='0'){
+            zeros++;
+           }
+           else{
+            ones--;
+           }
+           currscore=ones+zeros;
+            maxscore=Math.max(currscore,maxscore);
+        }
+
+        return maxscore;
+
     }
 }
