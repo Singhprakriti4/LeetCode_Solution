2f4924d43cfe8124484b5dfa6ff73e2fd1e70770
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-01-02 17:58:43 +0530

    Time: 6 ms (74.03%) | Memory: 85.8 MB (63.33%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
@@ -1,36 +1,26 @@
 class Solution {
     public int[] vowelStrings(String[] words, int[][] queries) {
-        int ans[]=new int[queries.length];
+     int ans[]=new int[queries.length];
      int[] prefixcount=new int[words.length];
 
      String s=words[0];
      
-      if(s.charAt(0)=='a' || s.charAt(0)=='e' || s.charAt(0)=='i' || 
-           s.charAt(0)=='o' || s.charAt(0)=='u'){
-            if(s.charAt(s.length()-1)=='a' || s.charAt(s.length()-1)=='e' || s.charAt(s.length()-1)==
-            'i' || s.charAt(s.length()-1)=='o' || s.charAt(s.length()-1)=='u'){
+       if(isVowel(s.charAt(0)) && isVowel(s.charAt(s.length()-1))){
                 prefixcount[0]=1;
-            }
+        
         }
-      
-
+        
      for(int i=1;i<words.length;i++){
         s=words[i];
-        if(s.charAt(0)=='a' || s.charAt(0)=='e' || s.charAt(0)=='i' || 
-           s.charAt(0)=='o' || s.charAt(0)=='u'){
-            if(s.charAt(s.length()-1)=='a' || s.charAt(s.length()-1)=='e' || s.charAt(s.length()-1)==
-            'i' || s.charAt(s.length()-1)=='o' || s.charAt(s.length()-1)=='u'){
+        if(isVowel(s.charAt(0)) && isVowel(s.charAt(s.length()-1))){
                 prefixcount[i]=prefixcount[i-1]+1;
-            }
-            else{
-              prefixcount[i]=prefixcount[i-1];  
-            }
-        }
+         }
         else{
              prefixcount[i]=prefixcount[i-1];
         }
-     }
-
+    }
+       
+     
      for(int i=0;i<queries.length;i++){
         if(queries[i][0]==0){
             ans[i]=prefixcount[queries[i][1]];
@@ -41,5 +31,8 @@ class Solution {
      }
 
      return ans;
+    }
+    boolean isVowel(char ch){
+        return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
     }
 }
