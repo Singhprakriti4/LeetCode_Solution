3be5e13aacbc4c81a0d584bf0c08226c62e380a5
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-01-06 09:43:32 +0530

    Time: 3 ms (94.79%) | Memory: 45 MB (32.47%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,20 +1,32 @@
 class Solution {
     public int[] minOperations(String boxes) {
-        char[] arr=boxes.toCharArray();
+     char[] arr=boxes.toCharArray();
+     int[] leftsum=new int[boxes.length()];
 
-        int[] answer=new int[boxes.length()];
-        TreeSet<Integer> set=new TreeSet<>();
-        
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]=='1'){
-                set.add(i);
-            }
+     int count=Integer.parseInt(Character.toString(arr[0]));
+     leftsum[0]=0;
+     for(int i=1;i<leftsum.length;i++){
+        leftsum[i]=leftsum[i-1]+count;
+        if(arr[i]=='1'){
+            count++;
         }
-        for(int i=0;i<arr.length;i++){
-            for(int j:set){
-                answer[i]+=Math.abs(i-j);
-            }
+     }
+
+     count=Integer.parseInt(Character.toString(arr[arr.length-1]));
+     int[] rightsum=new int[boxes.length()];
+     rightsum[rightsum.length-1]=0;
+     for(int i=rightsum.length-2;i>=0;i--){
+        rightsum[i]=rightsum[i+1]+count;
+        if(arr[i]=='1'){
+            count++;
         }
-        return answer;
+     }
+
+     int[] result=new int[boxes.length()];
+     for(int i=0;i<result.length;i++){
+        result[i]=leftsum[i]+rightsum[i];
+     }
+     
+     return result;
     }
 }
