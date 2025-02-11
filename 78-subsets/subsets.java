fb5486c245a6e177c29bead7e061ce1d12aec4ae
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-02-11 21:44:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (30.95%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,25 +1,20 @@
 class Solution {
+    //with backtracking
+    List<List<Integer>> ans=new ArrayList<>();
     public List<List<Integer>> subsets(int[] nums) {
-
-      List<List<Integer>> outerlist=new ArrayList<>();
-      List<Integer> ans=new ArrayList<>();
-      subs(nums,0,ans,outerlist);
-      return outerlist;
- 
+        ArrayList<Integer> list=new ArrayList<>();
+        ss(nums,0,list);
+        return ans;
     }
-    public void subs(int[] nums,int ptr,List<Integer> ans,List<List<Integer>> outerlist){
-
-        if(ptr==nums.length){
-            outerlist.add(new ArrayList<>(ans));
+    public void ss(int[] nums,int index,ArrayList<Integer> list){
+        if(index==nums.length){
+           ans.add(new ArrayList<>(list));
             return;
         }
-         
-        
-        ans.add(nums[ptr]);
-        subs(nums, ptr+1 , ans ,outerlist);
-        ans.remove(ans.size()-1);
-        subs(nums,ptr+1,ans,outerlist);
+        list.add(nums[index]);
+        ss(nums,index+1,list);
+        list.remove(list.size()-1);
+        ss(nums,index+1,list);
 
     }
-
 }
