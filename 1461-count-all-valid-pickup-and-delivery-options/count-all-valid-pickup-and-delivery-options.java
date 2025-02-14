9383fb007d5cad0ed4bc38503c1c48e1d2cd607a
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-02-14 09:44:44 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (32.65%) - LeetSync

--- 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
@@ -1,7 +1,6 @@
 class Solution {
     public int countOrders(int n) {
         int mod=1000000007;
-        int[] dp=new int[n+1];
          long count=1;
         for(int i=2;i<n+1;i++){
             int poss=2*i-1;
