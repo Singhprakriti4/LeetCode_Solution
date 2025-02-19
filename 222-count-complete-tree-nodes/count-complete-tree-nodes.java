ad60ff2672fa9b20fc3bbf2abc99681a7c126964
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-02-19 12:09:13 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (37.31%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -14,17 +14,47 @@
  * }
  */
 class Solution {
-    int count=0;
+    //less than order of n
+
     public int countNodes(TreeNode root) {
-      preorder(root);
-      return count;  
+        return func(root);
     }
-    void preorder(TreeNode root){
+    public int func(TreeNode root){
         if(root==null){
-            return;
+            return 0;
         }
-        count++;
-        preorder(root.left);
-        preorder(root.right);
+        //  height of left subtree(left and right)
+       int ans=1;
+
+       int llh= height(root.left,-1);
+       int lrh= height(root.left,1);
+
+       int rlh=height(root.right,-1);
+       int rrh=height(root.right,1);
+
+       if((llh==lrh) && (rrh==rlh)){
+        //both are balanced
+        ans=ans+(int)((int)Math.pow(2,llh)-1) +(int) ((int)Math.pow(2,rlh)-1);
+       }
+       else if(llh!=lrh){
+        ans=ans+((int)Math.pow(2,rlh)-1)+func(root.left);
+       }
+       else{
+        ans=ans+(int)((int)Math.pow(2,llh)-1)+func(root.right);
+       }
+      return ans;
+    }
+
+    public int height(TreeNode root,int path){
+        //path=-1--> left
+        //path=1--> right
+        if(root==null){
+            return 0;
+        }
+        
+        if(path==-1){
+           return 1+ height(root.left,-1);
+        }
+           return 1+ height(root.right,1);
     }
 }
