ccde01ecaef99cca9f4a919ef34b0e5b77241610
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-03-06 15:46:03 +0530

    Time: 1 ms (83.67%) | Memory: 41.2 MB (23.60%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -3,16 +3,15 @@ class Solution {
         if(x==0){
             return 0;
         }
-        int ans=x%10;
+        long ans=x%10;
         x=x/10;
         while(x!=0){
-             if(Math.abs(ans)>Integer.MAX_VALUE/10){
-                return 0;
-            }
             ans=ans*10+x%10;
             x=x/10;
         }
-       
-        return ans;
+        if(Math.abs(ans)>(long)Integer.MAX_VALUE){
+            return 0;
+        }
+        return (int)ans;
     }
 }
