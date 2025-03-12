e18fd44f6e297b289ddc50056112c3650dfdf748
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-03-12 09:25:46 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (36.64%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,20 +1,70 @@
 class Solution {
     public int maximumCount(int[] nums) {
-       int neg=0;
-       int zero=0;
-       for(int i=0;i<nums.length;i++){
-        if(nums[i]<0){
-            neg++;
-        }
-        if(nums[i]==0){
-            zero++;
-        }
-       }
+     //find the last and first zero in the array
+     int start=binary(nums,false);
+     int end=binary(nums,true);
 
-       int pos=nums.length-neg-zero;
-       if(neg>pos){
-        return neg;
+     int neg=start;
+     int pos=nums.length-end-1;
+
+     if(start==-1){
+       start=findneg(nums)+1;
+       if(start==-1){
+        neg=0;
+        pos=nums.length;
        }
-       return pos;
+       else{
+         neg=start;
+         pos=nums.length-neg;
+       }
+     }
+
+     if(neg>pos){
+        return neg;
+     }
+     return pos;
+
+    }
+    public int binary(int[] nums,boolean last){
+        int s=0;
+        int e=nums.length-1;
+        int ans=-1;
+
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]==0){
+                ans=mid;
+                if(last){
+                    s=mid+1;
+                }
+                else{
+                    e=mid-1;
+                }
+            }
+            else if(nums[mid]<0){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return ans;
+    }
+    public int findneg(int[] nums){
+        int s=0;
+        int e=nums.length-1;
+        int ans=-1;
+
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]<0){
+                ans=mid;
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return ans;
     }
 }
