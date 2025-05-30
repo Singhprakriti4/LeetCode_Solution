ad461c38c0bf25d6b0b03aaa2196567c1693be46
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-05-30 23:47:35 +0530

    Time: 95 ms (5.01%) | Memory: 45.2 MB (46.08%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,77 +1,52 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int[][] dp=new int[s.length()+1][p.length()+1];
-        //0-> not calculated
-        //1-> true
-        //2-> false
-        return match(0, 0, s, p, dp);
-    }
-    public boolean match(int i, int j, String s, String p, int[][] dp){
 
-        if(i==s.length() && j==p.length()) {
-            // System.out.println(s.charAt(i-1)+" "+p.charAt(j-1));
-            return true;
+        boolean[][] dp=new boolean[s.length()+1][p.length()+1];
+        
+        int n=s.length();
+        int m=p.length();
+
+        //initialization
+        dp[n][m]=true;
+        for(int i=0;i<n;i++){
+            dp[i][m]=false;
         }
-
-        if(i==s.length() && j!=p.length()){
-           
-            if(p.charAt(j)=='*') 
-            {    
-                
-                return match(i, j+1, s, p, dp);
-            }
-            else
-            { 
-                
-                return false;
-            }
-        }
-
-        if(i!=s.length() && j==p.length()) return false;
-
-        if(dp[i][j]!=0){
-            if(dp[i][j]==1) return true;
-            else return false;
-        }
-
-        if(p.charAt(j)!='?' && p.charAt(j)!='*'){
-           
-            if(s.charAt(i)!=p.charAt(j)){
-                //  System.out.println(s.charAt(i)+" "+p.charAt(j));
-                dp[i][j]=2;
-                 return false;
+        for(int i=m-1;i>=0;i--){
+            if(p.charAt(i)!='*'){
+                dp[n][i]=false;
             }
             else{
-            boolean res= match(i+1, j+1, s, p, dp);
-            if(res==true) dp[i][j]=1;
-            else dp[i][j]= 2;
-            return res;
+               dp[n][i]=dp[n][i+1];
             }
         }
 
-        if(p.charAt(j)=='?'){
-            // System.out.println(s.charAt(i)+" "+p.charAt(j));
-            boolean res=match(i+1, j+1, s, p, dp); 
-            if(res==true) dp[i][j]=1;
-            else dp[i][j]=2;
-            return res;
-        }
-        
-        if(p.charAt(j)=='*'){
-            boolean ans=false;
-            //try all different combinations
-            for(int leap=0; leap<=s.length()-i; leap++){
-                ans=ans || match( i+leap, j+1, s, p, dp);
-            }
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(p.charAt(j)!='?' && p.charAt(j)!='*'){
+                    //surely a character
+                    if(s.charAt(i)!=p.charAt(j)){
+                        dp[i][j]=false;
+                    }
+                    else{
+                        dp[i][j]=dp[i+1][j+1];
+                    }
+                }
+                if(p.charAt(j)=='?'){
+                    dp[i][j]=dp[i+1][j+1];
+                }
+                if(p.charAt(j)=='*'){
 
-            if(ans==true) {
-                dp[i][j]=1;
-                return ans;
+                    //iterate on each possibility
+                    for(int leap=i;leap<=n;leap++){
+                        if(dp[leap][j+1]==true){
+                            dp[i][j]=true;
+                            break;
+                        }
+                    }
+                }
             }
-
         }
-        
-        dp[i][j]=2;
-        return false;
+
+        return dp[0][0];
     }
 }
