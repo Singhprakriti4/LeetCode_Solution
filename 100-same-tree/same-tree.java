2c819492deee8a5f2969deca60a91cb760a11048
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-08-29 15:50:43 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (19.98%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -14,28 +14,20 @@
  * }
  */
 class Solution {
-    boolean ans=true;
     public boolean isSameTree(TreeNode p, TreeNode q) {
-       preorder(p,q);
-       return ans;
+        return helper(p,q);
     }
-    private void preorder(TreeNode p,TreeNode q){
-      if(p==null && q==null){
-        return;
-      }
-      else if(p==null || q==null){
-        ans=false;
-        return;
-      }
-      else if(p.val!=q.val){
-        ans=false;
-        return;
-      }
-      else{
-        preorder(p.left,q.left);
-        preorder(p.right,q.right);
-      }
-      
-      
+    public boolean helper(TreeNode p, TreeNode q){
+
+        if(p==null & q==null){
+            return true;
+        }
+        if( (p==null && q!=null) || (p!=null && q==null)){
+            return false;
+        }
+        if(p.val!=q.val){
+            return false;
+        }
+        return helper(p.left,q.left) && helper(p.right,q.right);
     }
 }
