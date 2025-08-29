672b469c52a30763bfbce42a406ea9e472ff2bd7
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-08-29 20:42:38 +0530

    Time: 2 ms (99.45%) | Memory: 42.7 MB (34.83%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -1,3 +1,4 @@
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -13,53 +14,50 @@
  *     }
  * }
  */
+
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
         List<List<Integer>> ans=new ArrayList<>();
-        Queue<Pair<TreeNode,Pair<Integer,Integer>>> q=new LinkedList<>();
-        TreeMap<Integer,TreeMap<Integer,ArrayList<Integer>>> m=new TreeMap<>();
-        if(root==null){
-            return ans;
-        }
-        q.add(new Pair<>(root,new Pair<>(0,0)));
-        while(!q.isEmpty()){
-            Pair<TreeNode,Pair<Integer,Integer>> p=q.poll();
-            //the node is the key for our outer pair
-            TreeNode n=p.getKey();
-            //the inner pair stores the row and the col of our node 'n'
-            int row=p.getValue().getKey();
-            int col=p.getValue().getValue();
-            //map me add krna properly
-           
-            if(m.containsKey(col)){
-                if(m.get(col).containsKey(row)){
-                  m.get(col).get(row).add(n.val);
-                }
-                else{
-                 m.get(col).put(row,new ArrayList<>(Arrays.asList(n.val)));
-                }
-            }
-            else{
-                TreeMap<Integer,ArrayList<Integer>> inner=new TreeMap<>();
-                inner.put(row, new ArrayList<>(Arrays.asList(n.val)));
-               m.put(col,inner);
-            }
-             if(n.left!=null)
-             q.add(new Pair<>(n.left,new Pair<>(row+1,col-1)));
-             if(n.right!=null)
-             q.add(new Pair<>(n.right,new Pair<>(row+1,col+1)));
-        }
+        TreeMap<Integer,TreeMap<Integer, ArrayList<Integer>>> outer=new TreeMap<>();
+        helper(outer, 0, 0, root);
 
-        for(Integer col: m.keySet()){
-         List<Integer> inner=new ArrayList<>();
-            for(Integer row : m.get(col).keySet()){
-               ArrayList<Integer> set=m.get(col).get(row);
-               Collections.sort(set);
-               inner.addAll(set);
+        //traverse on the set and fill in the values
+        for(Map.Entry<Integer, TreeMap<Integer, ArrayList<Integer>>> e: outer.entrySet()){
+            int col=e.getKey();
+            List<Integer> inner=new ArrayList<>();
+            for(Map.Entry<Integer, ArrayList<Integer>> in: e.getValue().entrySet() ){
+                int row=in.getKey();
+                ArrayList<Integer> values=in.getValue();
+                Collections.sort(values);
+                for(int i=0;i<values.size();i++){
+                    inner.add(values.get(i));
+                }
             }
             ans.add(inner);
         }
-      return ans;
+
+        return ans;
+
     }
-    
+    public void helper( TreeMap<Integer,TreeMap<Integer, ArrayList<Integer>>> outer
+    ,int row, int col, TreeNode root ){
+
+        if(root==null){
+            return;
+        }
+
+        if(!outer.containsKey(col)){
+           TreeMap<Integer, ArrayList<Integer>> inner=new TreeMap<>();
+           outer.put(col, inner);
+        }
+        if(!outer.get(col).containsKey(row)){
+            outer.get(col).put(row,new ArrayList<>());
+        }
+
+        outer.get(col).get(row).add(root.val);
+
+        helper(outer, row+1, col-1, root.left);
+        helper(outer, row+1, col+1, root.right);
+        
+    }
 }
