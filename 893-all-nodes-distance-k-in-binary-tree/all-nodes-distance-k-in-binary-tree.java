22387933999e1b2edb05f429c033bb6017a72ba5
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-08-31 18:13:23 +0530

    Time: 12 ms (90.51%) | Memory: 42.6 MB (74.93%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -8,50 +8,55 @@
  * }
  */
 class Solution {
-    List<Integer> ans=new ArrayList<>();
-    Map<TreeNode,TreeNode> m=new HashMap<>();
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-      parent(root);
-      dist(root,target,target,0,k);
-      return ans;
-    }
-    public void parent(TreeNode root){
-        if(root==null){
-            return;
-        }
-        if(root.left!=null){
-            m.put(root.left,root);
-        }
-        if(root.right!=null){
-            m.put(root.right,root);
-        }
-        parent(root.left);
-        parent(root.right);
-    }
-   
-    public void dist(TreeNode root,TreeNode target,TreeNode fixed,int curr_count,int req_count){
-        if(target==null){
-            return;
-        }
-       if(curr_count==req_count ){
-        ans.add(target.val);
-        return;
-       }
+        //child parent
+        HashMap<TreeNode,TreeNode> map=new HashMap<>();
+        helper(root, null,map );
+
+        //debugging helper
+        // for(Map.Entry<TreeNode,TreeNode> e: map.entrySet()){
+        //     System.out.println(e.getKey() +" "+ e.getValue());
+        //     System.out.println();
+        // }
+
+        List<Integer> ans=new ArrayList<>();
+        dfs(target, 0, k, map, ans );
+        return ans;
 
 
-    if(m.containsKey(target)){
-        TreeNode p=m.get(target);
-       if( p!=null && p!=fixed ){
-       dist(root,p,target,curr_count +1, req_count);
-       }
     }
-       
-       if(target.left!=fixed && target.left!=null ){
-       dist(root,target.left,target,curr_count+1,req_count);
-       }
-       
-       if(target.right!=fixed && target.right!=null){
-       dist(root,target.right,target,curr_count+1,req_count);
-       }
+    //to fill child-parent map
+    public void helper(TreeNode root, TreeNode prev, HashMap<TreeNode,TreeNode> map){
+        if(root==null ){
+            return;
+        }
+        map.put(root,prev);
+
+        helper(root.left, root, map);
+        helper(root.right, root, map);
     }
+
+    public void dfs(TreeNode root, int currdist, int target,
+    HashMap<TreeNode,TreeNode> map, List<Integer> ans){
+
+        if(root==null || root.val==-1 || currdist>target){
+            return;
+        }
+
+        if(currdist==target){
+            ans.add(root.val);
+            return;
+        }
+        //explore all 3 paths
+        int previousvalue=root.val;
+        root.val=-1;
+        if(map.containsKey(root)){
+            dfs(map.get(root),currdist+1, target, map, ans);
+        }
+            dfs(root.left, currdist+1, target, map, ans);
+            dfs(root.right, currdist+1, target, map, ans);
+
+        root.val=previousvalue;
+    }
+
 }
