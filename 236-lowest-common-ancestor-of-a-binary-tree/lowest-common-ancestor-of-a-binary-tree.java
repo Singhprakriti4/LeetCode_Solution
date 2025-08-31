ba4c9c04c0f5e93b4c490feefddc48145aa8a2c3
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-08-31 11:14:17 +0530

    Time: 91 ms (5.01%) | Memory: 47.3 MB (5.53%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -8,32 +8,51 @@
  * }
  */
 class Solution {
-    TreeNode ans=null;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-         helper(root, p, q); 
-         return ans;
+        ArrayList<TreeNode> list1=new ArrayList<>();
+        ArrayList<TreeNode> list2=new ArrayList<>();
+
+        ArrayList<TreeNode> curr1=new ArrayList<>();
+        ArrayList<TreeNode> curr2=new ArrayList<>();
+
+        dfs(root,p,curr1,list1);
+        dfs(root,q,curr2,list2);
+
+        int idx=0;
+        TreeNode ans=root;
+
+        for(int i=0;i< list1.size();i++){
+            System.out.print(list1.get(i)+" ");
+        }
+        System.out.println();
+         for(int i=0;i< list2.size();i++){
+            System.out.print(list2.get(i)+" ");
+        }
+
+        while( idx<list1.size() && idx<list2.size() && list1.get(idx)==list2.get(idx)){
+            ans=list1.get(idx);
+            idx++;
+        }
+       
+        return ans;
     }
-    public void helper(TreeNode root, TreeNode p, TreeNode q){
+    public void dfs(TreeNode root, TreeNode target, ArrayList<TreeNode> curr,
+    ArrayList<TreeNode> path){
+
         if(root==null){
             return;
         }
 
-        if(contains(root,p) && contains(root,q)){
-            ans=root;
+        curr.add(root);
+        if(root==target){
+            // path=new ArrayList<>(curr);
+            path.addAll(curr);
+            curr.remove(curr.size()-1);
+            return;
         }
 
-        helper(root.left,p,q);
-        helper(root.right,p,q);
-    }
-    public boolean contains(TreeNode root, TreeNode target){
-        if(root==null){
-            return false;
-        }
-
-        if(root.val==target.val){
-            return true;
-        }
-
-        return contains(root.left, target) || contains(root.right, target);
+        dfs(root.left, target, curr, path);
+        dfs(root.right, target, curr, path);
+        curr.remove(curr.size()-1);
     }
 }
