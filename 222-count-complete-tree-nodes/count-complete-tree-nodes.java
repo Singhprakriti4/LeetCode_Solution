fb9eed5897c98ebdb0359f073a76aefe45b707da
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-09-01 23:04:39 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (64.09%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -14,47 +14,11 @@
  * }
  */
 class Solution {
-    //less than order of n
-
     public int countNodes(TreeNode root) {
-        return func(root);
-    }
-    public int func(TreeNode root){
         if(root==null){
             return 0;
         }
-        //  height of left subtree(left and right)
-       int ans=1;
 
-       int llh= height(root.left,-1);
-       int lrh= height(root.left,1);
-
-       int rlh=height(root.right,-1);
-       int rrh=height(root.right,1);
-
-       if((llh==lrh) && (rrh==rlh)){
-        //both are balanced
-        ans=ans+(int)((int)Math.pow(2,llh)-1) +(int) ((int)Math.pow(2,rlh)-1);
-       }
-       else if(llh!=lrh){
-        ans=ans+((int)Math.pow(2,rlh)-1)+func(root.left);
-       }
-       else{
-        ans=ans+(int)((int)Math.pow(2,llh)-1)+func(root.right);
-       }
-      return ans;
-    }
-
-    public int height(TreeNode root,int path){
-        //path=-1--> left
-        //path=1--> right
-        if(root==null){
-            return 0;
-        }
-        
-        if(path==-1){
-           return 1+ height(root.left,-1);
-        }
-           return 1+ height(root.right,1);
+        return countNodes(root.left)+countNodes(root.right)+1;
     }
 }
