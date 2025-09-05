4f4c1caf5c67e66c04e88786f08d3cb0e21a6f78
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-09-06 00:17:32 +0530

    Time: 2 ms (82.94%) | Memory: 45.3 MB (85.34%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,47 +1,62 @@
 class Solution {
     public void solve(char[][] board) {
+       boolean[][] visited=new boolean[board.length][board[0].length];
 
-        boolean[][] tobechanged=new boolean[board.length][board[0].length];
-
-        for(int i=0;i<board.length;i++){
-            if(board[i][0]=='O' && !tobechanged[i][0]){
-            dfs(i,0,board,tobechanged);}
-
-            if(board[i][board[0].length-1]=='O' && !tobechanged[i][board[0].length-1]){
-            dfs(i,board[0].length-1,board,tobechanged);}
+       int row=0;
+       for(int i=0;i<board[0].length;i++){
+        if(board[row][i]=='O'){
+            dfs(board,visited,row,i);
         }
+       }
+
+       if(board.length>1){
+        row=board.length-1;
         for(int i=0;i<board[0].length;i++){
-            if(board[0][i]=='O' && !tobechanged[0][i]){
-            dfs(0,i,board,tobechanged);}
-
-            if(board[board.length-1][i]=='O' && !tobechanged[board.length-1][i]){
-            dfs(board.length-1,i,board,tobechanged);}
+        if(board[row][i]=='O'){
+            dfs(board,visited,row,i);
         }
-     
+       }
+       }
 
-     for(int i=0;i<board.length ;i++){
-        for(int j=0;j<board[i].length;j++){
-            if(board[i][j]=='O' && tobechanged[i][j]==false){
+       int col=0;
+       for(int i=0;i<board.length;i++){
+         if(board[i][col]=='O'){
+            dfs(board,visited,i,col);
+        }
+       }
+
+       if(board[0].length>1){
+        col=board[0].length-1;
+        for(int i=0;i<board.length;i++){
+         if(board[i][col]=='O'){
+            dfs(board,visited,i,col);
+        }
+       }
+       }
+
+     for(int i=0;i<board.length;i++){
+        for(int j=0;j<board[0].length;j++){
+            if(visited[i][j]==false){
                 board[i][j]='X';
             }
         }
      }
-
+       
     }
-    public void dfs(int row,int col,char[][] board , boolean[][] tobechanged){  
-      
-        tobechanged[row][col]=true;
 
-        int[] rowdiff={0,0,-1,1};
-        int[] coldiff={-1,1,0,0};
+    public void dfs(char[][] board, boolean[][] visited, int r, int c){
+        visited[r][c]=true;
+
+        int[] val1={0,0,-1,1};
+        int[] val2={-1,1,0,0};
 
         for(int i=0;i<4;i++){
-            int r=row+rowdiff[i];
-            int c=col+coldiff[i];
+            int row=r+val1[i];
+            int col=c+val2[i];
 
-            if(r>=0 && r<board.length && c>=0 && c<board[0].length 
-            && board[r][c]=='O' && !tobechanged[r][c]){
-                dfs(r,c,board,tobechanged);
+            if(row>=0 && col>=0 && row<board.length && col<board[0].length
+            && !visited[row][col] && board[row][col]=='O'){
+                dfs(board, visited, row, col);
             }
         }
     }
