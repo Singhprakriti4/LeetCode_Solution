faf3dc49320278598cb81f1910a6b7ffa4e53407
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-09-05 23:24:32 +0530

    Time: 16 ms (23.11%) | Memory: 48 MB (19.07%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,44 +1,54 @@
+/*
+1) why bfs?
+2) why multi source bfs the otherway?
+3) why did i not start the visited dist with 0 ?
+*/
 class Solution {
+
+    //multi source bfs.
+    //ripple effect . the closest 1. the zero closest to nay one will get the dist
     public int[][] updateMatrix(int[][] mat) {
-        int[][] ans=new int[mat.length][mat[0].length];
-        // boolean[][] visited=new boolean[mat.length][mat[0].length];
+
         Queue<int[]> q=new LinkedList<>();
+        int[][] result=new int[mat.length][mat[0].length];
+        //for visit tracking + distance storing
+        int[][] visited=new int[mat.length][mat[0].length];
 
         for(int i=0;i<mat.length;i++){
             for(int j=0;j<mat[0].length;j++){
+                result[i][j]=mat[i][j];
                 if(mat[i][j]==0){
-                    ans[i][j]=0;
-                    q.add(new int[] {i,j});
-                    // visited[i][j]=true;
-                }
-                else{
-                    ans[i][j]=Integer.MAX_VALUE;
+                    q.add(new int[]{i,j});
+                    visited[i][j]=1;
                 }
             }
         }
+
        
-       int[] rowdiff={-1,1,0,0};
-       int[] coldiff={0,0,-1,1};
+       
+        while(!q.isEmpty()){
+            int[] arr=q.poll();
+            int r=arr[0];
+            int c=arr[1];
 
-       while(!q.isEmpty()){
-        int[] arr=q.poll();
-        int r=arr[0];
-        int c=arr[1];
+            int[] val1={0,0,1,-1};
+            int[] val2={1,-1,0,0};
 
-        for(int i=0;i<4;i++){
-            int rownum=r+rowdiff[i];
-            int colnum=c+coldiff[i];
+            for(int i=0;i<4;i++){
+                int row=r+val1[i];
+                int col=c+val2[i];
 
-            if(rownum>=0 && rownum<mat.length && colnum>=0 &&
-            colnum<mat[0].length && ans[rownum][colnum]>ans[r][c]+1){
-                // visited[rownum][colnum]=true;
-                q.add(new int[]{rownum,colnum});
-                ans[rownum][colnum]=ans[r][c]+1;
+                if(row>=0 && col>=0 && row<mat.length && col<mat[0].length
+                && visited[row][col]==0){
+                    visited[row][col]=visited[r][c]+1;
+                    q.add(new int[]{row,col});
+                    if(mat[row][col]==1){
+                        result[row][col]=visited[row][col]-1;
+                    }
+                }
             }
         }
 
-     }
-     return ans;
-
+        return result;
     }
 }
