0b70b8754440763b5b1f7d511960e8d2bc3e0770
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-09-05 16:59:09 +0530

    Time: 1 ms (87.18%) | Memory: 47.8 MB (60.97%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,56 +1,28 @@
-/*
-bfs
-using Queue
-*/ 
-
 class Solution {
+    //dfs
     public int findCircleNum(int[][] isConnected) {
-        int n= isConnected.length;
-        int[] visited=new int[n+1];
-
-        //making the adjecency list
-        ArrayList<Integer>[] adj=new ArrayList[n+1];
-        adj[0]=new ArrayList<>();
-        
-        for(int i=0;i<isConnected.length;i++){
-            adj[i+1]=new ArrayList<>();
-            int row=i;
-            for(int col=0;col<n;col++){
-                if(isConnected[row][col]==1){
-                    adj[row+1].add(col+1);
-                }
-            }
-        }
-       
-
+        int n=isConnected.length;
+        boolean[] visited=new boolean[n];
         int ans=0;
-        //0 : not visited, 1: visited
-        for(int i=1;i<visited.length;i++){
-            
-            if(visited[i]==0){
+
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
                 ans+=1;
-                visited[i]=1;
-                bfs(adj,visited,i);
+                visited[i]=true;
+                dfs(isConnected, visited, i);
             }
         }
+
         return ans;
+
     }
-    public void bfs(ArrayList<Integer>[] adj, int[] visited, int src){
-        Queue<Integer> q=new LinkedList<>();
-        q.add(src);
 
-        while(!q.isEmpty()){
-            int city=q.poll();
-            // visited[city]=1;
-
-            //visit its unvisited neighbours
-            for(int i=0;i<adj[city].size();i++){
-                if(visited[adj[city].get(i)]==0){
-                    visited[adj[city].get(i)]=1;
-                    q.add(adj[city].get(i));
-                }
-            }
-            
+    public void dfs(int[][] isConnected, boolean[] visited, int src ){
+       for(int i=0;i<isConnected[src].length;i++){
+        if(isConnected[src][i]==1 && !visited[i]){
+            visited[i]=true;
+            dfs(isConnected, visited, i);
         }
+       }
     }
 }
