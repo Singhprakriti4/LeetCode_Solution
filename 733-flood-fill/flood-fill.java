4242f8efc63d866deae2179bc87a36964b6a8412
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-09-05 18:03:04 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (50.36%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,36 +1,28 @@
-//bfs
+//dfs
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+
         if(image[sr][sc]==color){
             return image;
         }
-        int ref=image[sr][sc];
+        dfs(image, sr, sc, color, image[sr][sc]);
 
-        Queue<int[]> q=new LinkedList<>();
+        return image;
+
+    }
+    public void dfs(int[][] image, int sr, int sc, int color, int ref){
+        image[sr][sc]=color;
         int[] val1={0,0,-1,1};
         int[] val2={1,-1,0,0};
 
-        q.add(new int[]{sr,sc});
-        image[sr][sc]=color;
+        for(int i=0;i<4;i++){
+            int row=sr+val1[i];
+            int col=sc+val2[i];
 
-        while(!q.isEmpty()){
-            int[] arr=q.poll();
-            int r=arr[0];
-            int c=arr[1];
-
-            //go in all 4 directions
-            for(int i=0;i<4;i++){
-                int row=val1[i]+r;
-                int col=val2[i]+c;
-
-                if(row>=0 && col>=0 && row<image.length 
-                && col<image[0].length && image[row][col]==ref){
-                    image[row][col]=color;
-                    q.add(new int[]{row,col});
-                }
+            if(row>=0 && col>=0 && row<image.length && col<image[0].length
+            && image[row][col]==ref){
+                dfs(image, row, col, color, ref);
             }
         }
-
-        return image;
     }
 }
