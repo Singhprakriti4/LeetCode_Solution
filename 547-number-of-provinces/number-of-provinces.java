81638af5aabffd4c0bc24f456881f0b5d0dfb50f
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-09-05 16:46:20 +0530

    Time: 1 ms (87.18%) | Memory: 48.5 MB (8.91%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,33 +1,43 @@
+/*
+bfs
+w/o making the adjecency list
+using Queue
+*/ 
 class Solution {
     public int findCircleNum(int[][] isConnected) {
+        int n= isConnected.length;
+        int[] visited=new int[n+1];
+
         int ans=0;
-        if(isConnected.length==0){
-            return ans;
-        }
-        boolean[] visited=new boolean[isConnected.length];
-        for(int i=0;i<visited.length;i++){
-            visited[i]=false;
-        }
-        for(int i=0;i<visited.length;i++){
-            if(!visited[i]){
-                ans++;
+        //0 : not visited, 1: visited
+        for(int i=1;i<visited.length;i++){
+            
+            if(visited[i]==0){
+                ans+=1;
+                visited[i]=1;
                 bfs(isConnected,visited,i);
             }
         }
         return ans;
     }
-    public void bfs(int[][] isConnected,boolean[] visited,int start ){
-       Queue<Integer> q=new LinkedList<>();
-       q.add(start);
-       visited[0]=true;
-       while(!q.isEmpty()){
-        int val=q.poll();
-        for(int i=0;i<isConnected.length;i++){
-          if(isConnected[val][i]==1 && !visited[i]){
-            visited[i]=true;
-            q.add(i);
-          }
+    public void bfs(int[][] isConnected, int[] visited, int src){
+        Queue<Integer> q=new LinkedList<>();
+        q.add(src);
+
+        while(!q.isEmpty()){
+            int city=q.poll();
+            // visited[city]=1;
+
+            //visit its unvisited neighbours
+            for(int i=0;i<isConnected[0].length;i++){
+                int row=city-1;
+                int col=i;
+
+                if(isConnected[row][col]==1 && visited[col+1]==0){
+                    visited[col+1]=1;
+                    q.add(col+1);
+                }
+            }
         }
-       }
     }
 }
