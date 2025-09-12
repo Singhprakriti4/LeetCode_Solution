095700253961e53ae2c3134109a755a6826b5474
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-09-12 22:17:17 +0530

    Time: 19 ms (46.72%) | Memory: 55.3 MB (88.65%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,36 +1,61 @@
 class Solution {
+    //cycle detection in directed graph (dfs)
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        HashSet<Integer> terminal=new HashSet<>();
-        boolean[] visited=new boolean[graph.length];
-        ArrayList<Integer> ans=new ArrayList<>();
 
-        for(int i=0;i<graph.length;i++){
-            if(graph[i].length==0){
-                terminal.add(i);
-            }
+        // boolean[] notsafe=new boolean[graph.length];
+        Set<Integer> safe=new HashSet<>();
+        Set<Integer> unsafe=new HashSet<>();
+
+        boolean[] visited=new boolean[graph.length];
+        boolean[] currpath=new boolean[graph.length];
+
+        for(int i=0;i<visited.length;i++){
+
+                if(hascycle(visited,currpath,i,graph,unsafe,safe)){
+                    unsafe.add(i);
+                }
+                else{
+                    safe.add(i);
+                }
         }
-        for(int i=0;i<graph.length;i++){
-           if( safe(i,graph,terminal,visited))
-           ans.add(i);
+
+        List<Integer> ans=new ArrayList<>();
+
+        for(int node: safe){
+
+            ans.add(node);
         }
-        Collections.sort(ans);
-        return ans;
+         Collections.sort(ans);
+         return ans;
+
     }
-    public boolean safe(int node,int[][] graph,HashSet<Integer> terminal,boolean[] visited){
-        if(terminal.contains(node)){
+    public boolean hascycle(boolean[] visited, boolean[] currpath, int src,
+     int[][] graph, Set<Integer> unsafe, Set<Integer> safe){
+        visited[src]=true;
+        currpath[src]=true;
+
+        // if(notsafe[src]) return true;
+        if(unsafe.contains(src)){
             return true;
         }
-        visited[node]=true;
-        for(int i=0;i<graph[node].length;i++){
-            if(visited[graph[node][i]]){
-                return false;
+        if(safe.contains(src)){
+            return false;
+        }
+        
+        for(int i=0;i<graph[src].length;i++){
+            int node=graph[src][i];
+             if(unsafe.contains(node)){
+                return true;
             }
-            if(!safe(graph[node][i],graph,terminal,visited)){
-                return false;
+            else if(visited[node] && currpath[node]==true){
+                return true;
+            }
+            else if(!visited[node]){
+                if(hascycle(visited,currpath,node,graph,unsafe,safe)) return true;
             }
         }
-        visited[node]=false;
-        terminal.add(node);
-        return true;
+
+        currpath[src]=false;
+        return false;
     }
 }
