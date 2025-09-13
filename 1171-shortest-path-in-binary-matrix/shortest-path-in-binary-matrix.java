a6702d2955ac4a55763818f4a3ff176c2a90b7c8
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-09-13 11:17:23 +0530

    Time: 14 ms (76.99%) | Memory: 45.7 MB (52.94%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,44 +1,37 @@
-class Pair{
-    int row;
-    int col;
-    Pair(int row,int col){
-        this.row=row;
-        this.col=col;
-    }
-}
 class Solution {
+    //recursion
     public int shortestPathBinaryMatrix(int[][] grid) {
-        // ans=-1;
-        // boolean[] visited=new boolean[grid.length]
-        Queue<Pair> q=new LinkedList<>();
-        if(grid[0][0]==1 || grid[grid.length-1][grid[0].length-1]==1){
-            return -1;
-        }
-        q.add(new Pair(0,0));
-        grid[0][0]=1;
+        int ans=-1;
+        Queue<int[]> q=new LinkedList<>();
+        if(grid[0][0]!=0) return -1;
+
+        q.add(new int[]{0,0,1});
+        grid[0][0]=-1;
+
         while(!q.isEmpty()){
-           Pair p=q.poll();
-           int r=p.row;
-           int c=p.col;
-
-           int[] rowadd={0,1,1,-1,-1,1,0 ,-1};
-           int[] coladd={1,1,0, 1,0 ,-1,-1,-1};
-
-           for(int i=0;i<8;i++){
-            int curr_row=rowadd[i]+r;
-            int curr_col=coladd[i]+c;
-
-            if(curr_row>=0 && curr_row<grid.length && curr_col>=0 && curr_col<grid[0].length && grid[curr_row][curr_col]==0){
-               q.add(new Pair(curr_row,curr_col));
-              
-               grid[curr_row][curr_col]=grid[r][c]+1;
+            int[] arr=q.poll();
+            int row=arr[0];
+            int col=arr[1];
+            int dist=arr[2];
+            if(row==grid.length-1 && col==grid[0].length-1){
+                return dist;
             }
-           }
 
+            //move in 8 directions
+            int[] val1={-1,-1,-1,1,1,1,0,0};
+            int val2[]={-1,0,1,-1,0,1,-1,1};
+
+            for(int i=0;i<8;i++){
+                int r=row+val1[i];
+                int c=col+val2[i];
+
+                if(r>=0 && c>=0 && r<grid.length && c<grid[0].length
+                && grid[r][c]==0){
+                    q.add(new int[]{r,c,dist+1});
+                    grid[r][c]=-1;
+                }
+            }
         }
-        if(grid[grid.length-1][grid[0].length-1]==0){
-            return -1;
-        }
-        return grid[grid.length-1][grid[0].length-1];
+        return -1;
     }
 }
