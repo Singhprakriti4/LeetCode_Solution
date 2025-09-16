5ae851e60373ce548a7efdc537919ce8b4b8da8f
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-09-16 19:06:08 +0530

    Time: 56 ms (41.21%) | Memory: 46.2 MB (65.66%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,46 +1,29 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        
         int sum=0;
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
         }
         if(sum%2!=0) return false;
-
-        int[][] dp=new int[sum+1][nums.length+1];
-
-        return ans(nums,sum/2,nums.length-1,dp);
-
-
-
-    }
-    public boolean ans(int[] nums, int sum, int idx, int[][] dp){
-        if(sum==0){
-                return true;
-            }
-        if(idx==-1){
-            return false;
-        }
         
-        if(dp[sum][idx+1]!=0){
-            if(dp[sum][idx+1]==1) return true;
-            return false;
+        //base case-> initialisation
+
+        boolean[][] dp=new boolean[sum/2+1][nums.length+1];
+        for(int i=0;i<dp[0].length;i++){
+            dp[0][i]=true;
         }
 
-        boolean  res =false;
-        if(nums[idx]<=sum){
-         res= ans(nums,sum-nums[idx],idx-1,dp) ||
-               ans(nums,sum,idx-1,dp);
-        }
-        else{
-            res=ans(nums,sum,idx-1,dp);
+        for(int s=1;s<dp.length;s++){
+            for(int i=1;i<dp[0].length;i++){
+                if(nums[i-1]<=s){
+                    dp[s][i]=dp[s-nums[i-1]][i-1]|| dp[s][i-1];
+                }
+                else{
+                    dp[s][i]=dp[s][i-1];
+                }
+            }
         }
 
-        if(res){ dp[sum][idx+1]=1;
-        }
-        else{
-            dp[sum][idx+1]=2;
-        }
-        return res;
+        return dp[sum/2][nums.length];
     }
-} 
+}
