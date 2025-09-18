733579be14b8abeb35ffc62d33aef64a0d751960
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-09-18 12:32:54 +0530

    Time: 4 ms (91.28%) | Memory: 44.5 MB (36.60%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -5,19 +5,27 @@ class Solution {
         range+=nums[i];
        }
        if(Math.abs(target)>range || (target+range)%2==1) return 0;
-       System.out.println(Math.abs(range-target)/2);
-        return sum(nums,nums.length-1,Math.abs(range+target)/2);
+    //    System.out.println(Math.abs(range-target)/2);
+    int find=Math.abs(range+target)/2;
+        int[][] dp=new int[nums.length+1][find+1];
+        for(int i=0;i<dp.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return sum(nums,nums.length-1,find,dp);
     }
-    public int sum(int[] nums,int ptr,int value){
+    public int sum(int[] nums,int ptr,int value,int[][] dp){
         if(ptr==-1){
             if(value==0){
                 return 1;
             }
             return 0;
         }
+        if(dp[ptr+1][value]!=-1){
+            return dp[ptr+1][value];
+        }
        if(nums[ptr]<=value){
-        return sum(nums,ptr-1,value-nums[ptr])+sum(nums,ptr-1,value);
+        return dp[ptr+1][value]=sum(nums,ptr-1,value-nums[ptr],dp)+sum(nums,ptr-1,value,dp);
        }
-       return sum(nums,ptr-1,value);
+       return dp[ptr+1][value]=sum(nums,ptr-1,value,dp);
     }
 }
