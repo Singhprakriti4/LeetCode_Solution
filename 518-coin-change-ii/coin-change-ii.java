612fc57d27a5681c23f3e32cd8c67b7bf89ee5f7
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-09-24 23:37:29 +0530

    Time: 11 ms (34.52%) | Memory: 53.6 MB (5.05%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,27 +1,22 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int[][] dp=new int[coins.length+1][amount+1];
+
         for(int i=0;i<dp.length;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return func(coins,coins.length-1,amount,dp);
-    }
-    public int func(int[] coin,int idx, int amount, int[][] dp){
-        if(amount==0){
-            return 1;
-        }
-        if(idx==-1){
-            return 0;
-        }
+            dp[i][0]=1;
+                }
 
-        if(dp[idx+1][amount]!=-1){
-            return dp[idx+1][amount];
+        for(int idx=1;idx<dp.length;idx++){
+            for(int a=1;a<dp[0].length;a++){
+                if(coins[idx-1]<=a){
+                    dp[idx][a]=dp[idx][a-coins[idx-1]]+dp[idx-1][a];
+                }
+                else{
+                    dp[idx][a]=dp[idx-1][a];
+                }
+            }
         }
-        //takable
-        if(coin[idx]<=amount){
-            return dp[idx+1][amount]=func(coin,idx,amount-coin[idx],dp)+func(coin,idx-1,amount,dp);
-        }
-
-        return func(coin,idx-1,amount,dp);
+        // return func(coins,coins.length-1,amount,dp);
+        return dp[dp.length-1][dp[0].length-1];
     }
 }
