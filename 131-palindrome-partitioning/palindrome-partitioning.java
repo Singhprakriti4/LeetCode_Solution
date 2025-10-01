050d65830e47c72eb92af0a11050dbf82d82b191
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-10-01 18:11:08 +0530

    Time: 17 ms (5.51%) | Memory: 55.8 MB (98.52%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,26 +1,41 @@
 class Solution {
     List<List<String>> ans=new ArrayList<>();
+
     public List<List<String>> partition(String s) {
-        ArrayList<String> list=new ArrayList<>();
-        part(s,list);
+
+        List<String> temp=new ArrayList<>();
+         rec(s,0,0,temp);
+
         return ans;
+        
     }
-    public void part(String s,ArrayList<String> list){
+    public void rec(String s, int start, int i, List<String> temp){
         if(s.length()==0){
-            ans.add(new ArrayList<>(list));
+           ans.add(new ArrayList<>(temp));
+           return; 
+        }
+        if(i==s.length()){
+            //everything processed
+            // ans.add(new ArrayList<>(temp));
             return;
         }
-        for(int i=1;i<=s.length();i++){
-            if(isPalindrome(s.substring(0,i))){
-            list.add(s.substring(0,i));
-            part(s.substring(i,s.length()),list);
-            list.remove(list.size()-1);
-            }
+
+        //if palindrome found
+        if(isPalindrome(s,start,i)){
+            temp.add(s.substring(start,i+1));
+            rec(s.substring(i+1,s.length()),0,0,temp);
+            temp.remove(temp.size()-1);
         }
 
+        //move forward in the same string
+        rec(s,start,i+1,temp);
     }
-    public boolean isPalindrome(String s){
-        if(s.equals(new StringBuilder(s).reverse().toString())){
+
+    public boolean isPalindrome(String s, int b, int e){
+       String s1=s.substring(b,e+1);
+       String s2 = new StringBuilder(s.substring(b, e + 1)).reverse().toString();
+       
+        if(s1.equals(s2)){
             return true;
         }
         return false;
