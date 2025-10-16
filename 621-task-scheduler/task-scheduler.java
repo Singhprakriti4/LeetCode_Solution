97082e1bc181d484afc0e64fed47b14a6b60ab55
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-10-16 10:58:19 +0530

    Time: 63 ms (5.01%) | Memory: 45.9 MB (52.77%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -12,17 +12,23 @@ class Solution {
 
         //maxheap
         PriorityQueue<Character> maxheap=new PriorityQueue<>((a,b)->freq.get(b)-freq.get(a));
+       
 
-        maxheap.addAll(freq.keySet());
+        maxheap.addAll(freq.keySet());//O(k) only since ye ek baar me saare elemnts daala ke maxheapify call kar rha hai
         int lastideal=0;
         int steps=0;
+
+        // it may seem like the TC is O(n*k log k)
+        // but if you look at it closely
+        // the max no of inserts or poll is n times (for each charactere)
+        // so,total TC is O(nlogk)
         while(!maxheap.isEmpty()){
            //get the top n  and reduce their freq in the map ans if their freq is not equal to 0 the  add them back in the heap
            List<Character> local=new ArrayList<>();
            int k=n+1;
-           while(k!=0){
+           while(k!=0){//O(n+1) or fewer
             if(maxheap.size()==0) break;
-            char key=maxheap.poll();
+            char key=maxheap.poll();//O(log k)
             freq.put(key,freq.get(key)-1);
             if(freq.get(key)!=0){
                 local.add(key);
@@ -36,7 +42,7 @@ class Solution {
            }
 
            //add them abck in the heap
-           maxheap.addAll(local);
+           maxheap.addAll(local);//O(k)
 
         }
 
