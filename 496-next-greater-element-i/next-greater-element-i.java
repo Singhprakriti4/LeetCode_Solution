6a5012ba2290f940485d62d8b5ab0bf1b5d965bc
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-10-27 12:59:01 +0530

    Time: 4 ms (34.50%) | Memory: 44.7 MB (13.39%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        
         int[] ans=new int[nums1.length];
         
         //i need a way to store the next greater elemnt of all th
@@ -9,14 +10,10 @@ class Solution {
         Map<Integer,Integer> map=new HashMap<>();
 
         for(int i=0;i<nums2.length;i++){
-            if(stk.isEmpty()){
-                stk.push(i);
-            }
-            else if(nums2[i]<nums2[stk.peek()]){
+            if(stk.isEmpty() || nums2[i]<nums2[stk.peek()]){
                 stk.push(i);
             }
             else{
-            
                 while(!stk.isEmpty() && nums2[i]>nums2[stk.peek()]){
                     //since numbers are unique there fore no '=' case will apper
                     //and there will not be overwritten value for any key
@@ -25,12 +22,14 @@ class Solution {
                 stk.push(i);
             }
         }
-        while(!stk.isEmpty()){
-            map.put(nums2[stk.pop()],-1);
-        }
 
         for(int i=0;i<nums1.length;i++){
+            if(!map.containsKey(nums1[i])){
+                ans[i]=-1;
+            }
+            else{
             ans[i]=map.get(nums1[i]);
+            }
         }
 
         return ans;
