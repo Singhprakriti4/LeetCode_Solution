81e4c3dcfd9b5e1a204f888c536e346006ae1e24
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-10-27 12:57:02 +0530

    Time: 4 ms (34.50%) | Memory: 44.6 MB (20.34%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,37 +1,38 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-       int[] ans=new int[nums1.length];
-       int[] allbig=new int[nums2.length];
-       allbig=allnextbigele(nums2,allbig);
-       int k=0;
-       for(int i=0;i<nums1.length;i++){
-         int j=0;
-         while(nums1[i]!=nums2[j]){
-            j++;
-         }
-        ans[k]=allbig[j];
-        k++;
-       }
-       return ans;
+        int[] ans=new int[nums1.length];
+        
+        //i need a way to store the next greater elemnt of all th
+        //elrmrnts in arr2 in one oteration
 
-    }
-    public int[] allnextbigele(int[] nums2,int[] allbig){
-        int k=allbig.length-1;
-        Stack<Integer> s=new Stack<>();
-        for(int i=nums2.length-1;i>=0;i--){
-            while(!s.isEmpty() && s.peek()<=nums2[i]){
-                s.pop();
+        Stack<Integer> stk=new Stack<>();
+        Map<Integer,Integer> map=new HashMap<>();
+
+        for(int i=0;i<nums2.length;i++){
+            if(stk.isEmpty()){
+                stk.push(i);
             }
-            if(s.isEmpty()){
-                allbig[k]=-1;
+            else if(nums2[i]<nums2[stk.peek()]){
+                stk.push(i);
             }
             else{
-               allbig[k]=s.peek();
+            
+                while(!stk.isEmpty() && nums2[i]>nums2[stk.peek()]){
+                    //since numbers are unique there fore no '=' case will apper
+                    //and there will not be overwritten value for any key
+                    map.put(nums2[stk.pop()],nums2[i]);
+                }
+                stk.push(i);
             }
-            k--;
-            s.push(nums2[i]);
         }
-        return allbig;
-    }
+        while(!stk.isEmpty()){
+            map.put(nums2[stk.pop()],-1);
+        }
 
+        for(int i=0;i<nums1.length;i++){
+            ans[i]=map.get(nums1[i]);
+        }
+
+        return ans;
+    }
 }
