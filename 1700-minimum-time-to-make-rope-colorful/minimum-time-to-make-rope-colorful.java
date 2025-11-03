237e6e5f0d3b1ba4a43fe924c14712f2ffbc3f36
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-03 13:44:28 +0530

    Time: 10 ms (32.90%) | Memory: 93.1 MB (5.68%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
@@ -1,30 +1,22 @@
 class Solution {
-    public int minCost(String colors, int[] neededTime) {
+    public int minCost(String colors, int[] neededtime) {
+        int totaltime=neededtime[0];
+        int timetoreduce=0;
+        int currmax=neededtime[0];
 
-      int time=0;
-      Stack<Integer> stk=new Stack<>();
-
-      for(int i=0;i<colors.length();i++){
-        if(!stk.isEmpty() && colors.charAt(stk.peek())==colors.charAt(i)){
-            //keep the one with maximum time in stack
-            int time1=neededTime[stk.peek()];
-            int time2=neededTime[i];
-
-            if(time1<time2){
-                time+=time1;
-                stk.pop();
-                stk.push(i);
+        for(int i=1;i<colors.length();i++){
+            totaltime+=neededtime[i];
+            if(colors.charAt(i)==colors.charAt(i-1)){
+                //update the max in the cong. same colors
+                currmax=Math.max(currmax,neededtime[i]);
             }
             else{
-                time+=time2;
+                //prev chain of same colors ended
+                timetoreduce+=currmax;
+                currmax=neededtime[i];
             }
         }
-        else{
-            stk.push(i);
-        }
-      }
 
-      return time;
-
+        return totaltime-timetoreduce-currmax;
     }
 }
