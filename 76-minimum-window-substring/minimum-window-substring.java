923da338fed7f1affe955c6bd50d7d3bc8065c3c
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-04 19:46:26 +0530

    Time: 121 ms (10.22%) | Memory: 48.4 MB (5.60%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,58 +1,52 @@
 class Solution {
     public String minWindow(String s, String t) {
 
-        if(t.length()>s.length()) {
-        return "";
-       }
-
         int length=Integer.MAX_VALUE;
         String ans="";
-        // System.out.println(s.length());
-        // System.out.println(t.length());
 
         Map<Character, Integer> need=new HashMap<>();
 
         for(int i=0;i<t.length();i++){
             need.put(t.charAt(i), need.getOrDefault(t.charAt(i),0)+1);
         }
-        // System.out.println(need.size());
-
         int found=0;
 
         Map<Character, Integer> currcount=new HashMap<>();
         int left=0;
 
         for(int right=0;right<s.length();right++){
-         
+            
             char currchar=s.charAt(right);
 
+            if(need.containsKey(currchar)){
             currcount.put(currchar, currcount.getOrDefault(currchar,0)+1);
-
-            if(need.containsKey(currchar) && currcount.get(currchar).equals(need.get(currchar))){
+            if(currcount.get(currchar).equals(need.get(currchar))){
                 found+=1;
-                System.out.println(found);
             }
-
+            //condition matched
             while(found==need.size()){
-                // System.out.println("hi");
-                if(right-left+1<=length){
-                    ans=s.substring(left, right+1);
+
+                if(right-left+1<length){
                     length=right-left+1;
+                    ans=s.substring(left, right+1);
                 }
 
                 char totest=s.charAt(left);
 
-                if(need.containsKey(totest)){
+                if(currcount.containsKey(totest)){
 
                     currcount.put(totest, currcount.get(totest)-1);
                     //update the currcount and found if necessary
-
                     if(currcount.get(totest)<need.get(totest)){
                         found-=1;
+                        if(currcount.get(totest)==0){
+                        currcount.remove(totest);
+                    }
                     }
                 }
 
                 left++;
+            }
             }
         }
         return ans;
