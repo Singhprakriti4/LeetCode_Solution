9bfb8d1a505c4b27f1476f91f6e494cda9a77a9c
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-05 22:49:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (5.23%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,24 +1,22 @@
 class Solution {
+
     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return noofways(m, n, 0, 0, dp);
-    }
-    public int noofways(int m, int n, int r, int c, int[][] dp){
-        if(r<0 || r>=m || c<0 || c>=n){
-            return 0;
-        }
-        if(r==m-1 && c==n-1){
-            return 1;
-        }
-        if(dp[r][c]!=-1){
-            return dp[r][c];
-        }
-        int path1=noofways(m, n, r+1, c, dp);//down
-        int path2=noofways(m, n, r, c+1, dp);//right
 
-        return dp[r][c]= path1+path2;
+        for(int i=0;i<=m-1;i++){
+            dp[i][n-1]=1;
+        }
+        for(int i=0;i<=n-1;i++){
+            dp[m-1][i]=1;
+        }
+
+        for(int row=m-2;row>=0;row--){
+            for(int col=n-2;col>=0;col--){
+                dp[row][col]=dp[row+1][col]+dp[row][col+1];
+            }
+        }
+
+        return dp[0][0];
     }
+
 }
