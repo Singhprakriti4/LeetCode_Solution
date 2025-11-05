9a9081a77b58780ffe7d0b74ce9a04763875b0f4
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-05 23:28:50 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (5.13%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -2,28 +2,26 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
-        int[][] dp=new int[m][n];
+        int[][] dp=new int[m+1][n+1];
 
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
+        dp[m-1][n-1]=1;
+
+
+        for(int row=m-1;row>=0;row--){
+            for(int col=n-1;col>=0;col--){
+                if(obstacleGrid[row][col]==1){
+                    dp[row][col]=0;
+                    continue;
+                }
+                 if(row==m-1 && col==n-1){
+                    dp[row][col]=1;
+                    continue;
+                }
+                dp[row][col]=dp[row+1][col]+dp[row][col+1];
+            }
         }
 
-        return noofways(obstacleGrid, m, n, 0, 0, dp);
-    }
-    public int noofways(int[][] grid, int m, int n, int r, int c, int[][] dp){
-        if(r>=m || c>=n || grid[r][c]==1){
-            return 0;
-        }
-        if(r==m-1 && c==n-1){
-            return 1;
-        }
-        if(dp[r][c]!=-1){
-            return dp[r][c];
-        }
+        return dp[0][0];
 
-        int move1=noofways(grid, m, n, r+1, c, dp);//down
-        int move2=noofways(grid, m, n, r, c+1, dp);//right
-
-        return dp[r][c]=move1+move2;
     }
 }
