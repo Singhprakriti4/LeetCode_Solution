ab64584fe5bcdb1fcaaf1bc9f0ccc538fe224427
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-08 03:05:04 +0530

    Time: 1 ms (79.29%) | Memory: 43.3 MB (11.69%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,40 +1,28 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][] dp=new int[prices.length][2];
-
-        for(int i=0;i<prices.length;i++){
-            Arrays.fill(dp[i],Integer.MIN_VALUE);
-        }
-        return profit(prices, 0, false, dp);
-    }
-    public int profit(int[] prices,int idx, boolean bought,
-    int[][] dp){
-        // if(idx>prices.length)
-        if(idx>=prices.length){
-            return 0;
-        }
-        int j=0;
-        if(bought) j=1;
-        if(dp[idx][j]!=Integer.MIN_VALUE){
-
-            return dp[idx][j];
-
+        int[][] dp=new int[prices.length+1][2];
+        
+        //initialisation
+        for(int i=0;i<2;i++){
+            dp[prices.length][i]=0;//we reached the last case
         }
 
-        if(!bought){
-            int buynow=-prices[idx]+profit(prices, idx+1, 
-            true, dp);
-            int donotbuy=profit(prices, idx+1, false, dp);
+        for(int row=prices.length-1;row>=0;row--){
+            int truecol=1;
+            int falsecol=0;
 
-            return dp[idx][j]=Math.max(buynow, donotbuy);
+            dp[row][falsecol]=Math.max(-prices[row]+dp[row+1][1],
+                                       dp[row+1][0]);
+
+             int val=prices[row];
+             if(row+2<=prices.length){
+                val+=dp[row+2][0];
+             }
+
+             dp[row][truecol]=Math.max(val, dp[row+1][1]);
         }
-       
-        // int sellnow=0;
-        // if(idx+2<=prices.length)
-        int sellnow=prices[idx]+profit(prices, idx+2, false, dp);
-        int donotsell=profit(prices, idx+1, true, dp);
 
-        return dp[idx][j]=Math.max(sellnow, donotsell);
+        return dp[0][0];
 
     }
 }
