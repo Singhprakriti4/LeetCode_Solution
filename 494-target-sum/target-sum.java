cf4f8afa7eb5e58de6e9f493285bb91b1433cedb
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-07 15:30:34 +0530

    Time: 628 ms (18.31%) | Memory: 42.9 MB (58.68%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,31 +1,17 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-       int range=0;
-       for(int i=0;i<nums.length;i++){
-        range+=nums[i];
-       }
-       if(Math.abs(target)>range || (target+range)%2==1) return 0;
-    //    System.out.println(Math.abs(range-target)/2);
-    int find=Math.abs(range+target)/2;
-        int[][] dp=new int[nums.length+1][find+1];
-        for(int i=0;i<dp.length;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return sum(nums,nums.length-1,find,dp);
+        return ways(nums, target, nums.length-1);
     }
-    public int sum(int[] nums,int ptr,int value,int[][] dp){
-        if(ptr==-1){
-            if(value==0){
-                return 1;
+    public int ways(int[] nums, int target, int idx){
+        if(idx==-1){
+            //reached the last index
+            if(target==0){
+            return 1;
             }
             return 0;
         }
-        if(dp[ptr+1][value]!=-1){
-            return dp[ptr+1][value];
-        }
-       if(nums[ptr]<=value){
-        return dp[ptr+1][value]=sum(nums,ptr-1,value-nums[ptr],dp)+sum(nums,ptr-1,value,dp);
-       }
-       return dp[ptr+1][value]=sum(nums,ptr-1,value,dp);
+
+        return ways(nums,target-nums[idx],idx-1)+ways(nums, target+nums[idx], idx-1);
+
     }
 }
