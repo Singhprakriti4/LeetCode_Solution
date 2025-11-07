0f0622b3bcd1b8246f3e064d65ada7ba3e9e105b
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-08 00:22:12 +0530

    Time: 7 ms (40.06%) | Memory: 47.7 MB (17.40%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,34 +1,34 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
-        HashMap<String,Integer> map=new HashMap<>();
+        int[][][] dp=new int[prices.length+1][k+1][2];
+        //idx, chances left, bought(1) notbought(0)
 
-        return profit(prices, 0, k, false, map);
-    }
-    public int profit(int[] prices, int idx, int chances,boolean bought,
-    HashMap<String,Integer> map){
-
-        if(chances==0){
-            return 0;
-        }
-        if(idx==prices.length){
-            return 0;
-        }
-        String key=idx+":"+chances+":"+bought;
-        if(map.containsKey(key)){
-            return map.get(key);
-        }
-        if(!bought){
-            int buy=-prices[idx]+profit(prices, idx+1, chances, true, map);
-            int notbuy=profit(prices, idx+1, chances, false, map);
-
-            map.put(key, Math.max(buy,notbuy));
-            return map.get(key);
+        for(int i=0;i<k+1;i++){
+            for(int j=0;j<2;j++){
+                dp[prices.length][i][j]=0;//reached the end
+            }
         }
 
-        int sell=prices[idx]+profit(prices, idx+1, chances-1, false, map);
-        int notsell=profit(prices, idx+1, chances, true, map);
+        for(int i=0;i<=prices.length;i++){
+            for(int j=0;j<2;j++){
+                dp[i][0][j]=0;//if no chnaces left return 0
+            }
+        }
 
-        map.put(key, Math.max(sell, notsell));
-        return map.get(key);
+        for(int row=prices.length-1;row>=0;row--){
+            for(int col=1;col<=k;col++){
+                int trueval=1;
+                int falseval=0;
+
+                dp[row][col][falseval]=Math.max(dp[row+1][col][1]-prices[row],
+                                                dp[row+1][col][0]);
+                dp[row][col][trueval]=Math.max(dp[row+1][col-1][0]+prices[row],
+                                               dp[row+1][col][1]);
+            }
+        }
+
+        return dp[0][k][0];
+
+        
     }
 }
