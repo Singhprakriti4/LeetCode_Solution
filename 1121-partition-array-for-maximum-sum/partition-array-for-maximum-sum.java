f4d2aaf3ebbbfdc42e7ce6dc6d54af190f79a173
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-12 03:16:13 +0530

    Time: 6 ms (94.11%) | Memory: 43.9 MB (10.87%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -1,17 +1,40 @@
 class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
-        int[][] dp=new int[arr.length][k+1];
-        for(int i=0;i<dp.length;i++){
-            Arrays.fill(dp[i],-1);
-        }
+    //    //id dp intuition
+    //    int[] dp=new int[arr.length+1];
+    //    dp[arr.length]=0;
+    //    dp[arr.length-1]=arr[arr.length-1];
+    //    //idx represents the max no of subarrys starting fom the index idx
+    //    for(int idx=dp.length-2;idx>=0;idx--){
+    //     //we keep a track of maxcurrval and maxlength not exceeding k and 
+    //     // the index not exceeding the arr.length
+    //     int ans=0;
+    //     int currmax=0;
+    //     int currans=0;
+    //     for(int j=idx;j<idx+k;j++){
+    //         if(j>=arr.length) break;
+
+    //         currmax= Math.max(currmax, arr[j]);
+    //         currans=Math.max(currans, currmax*(j-idx+1)+dp[idx+1]);
+    //         ans=Math.max(ans, currans);
+    //     }
+    //     dp[idx]=ans;
+    //    }
+
+    //    return dp[0];
+
+        int[]dp=new int[arr.length];
+        // for(int i=0;i<dp.length;i++){
+            Arrays.fill(dp,-1);
+        // }
         return maxsum(arr, 0, k, dp);
     }
-    public int maxsum(int[] arr, int s, int k, int[][] dp){
+    public int maxsum(int[] arr, int s, int k, int[] dp){
         if(s==arr.length){
             return 0;
         }
-        if(dp[s][k]!=-1){
-            return dp[s][k];
+        if(dp[s]!=-1){
+            return dp[s];
         }
         int ans=0;
         int currmaxval=-1;
@@ -21,9 +44,8 @@ class Solution {
             currmaxval=Math.max(currmaxval,arr[i]);
             int currlen=i-s+1;
             int currans=currlen*currmaxval + maxsum(arr, i+1, k, dp);
-
             ans=Math.max(ans,currans);
         }
-        return dp[s][k]=ans;
+        return dp[s]=ans;
     }
 }
