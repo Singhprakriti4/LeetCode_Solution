633f502ce6b0f31154375d161f8df3121c67628e
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-12 03:21:25 +0530

    Time: 6 ms (94.11%) | Memory: 43.3 MB (19.70%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -1,33 +1,36 @@
 class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
-    //    //id dp intuition
-    //    int[] dp=new int[arr.length+1];
-    //    dp[arr.length]=0;
-    //    dp[arr.length-1]=arr[arr.length-1];
-    //    //idx represents the max no of subarrys starting fom the index idx
-    //    for(int idx=dp.length-2;idx>=0;idx--){
-    //     //we keep a track of maxcurrval and maxlength not exceeding k and 
-    //     // the index not exceeding the arr.length
-    //     int ans=0;
-    //     int currmax=0;
-    //     int currans=0;
-    //     for(int j=idx;j<idx+k;j++){
-    //         if(j>=arr.length) break;
+       int[] dp=new int[arr.length+1];
+       dp[arr.length]=0;
 
-    //         currmax= Math.max(currmax, arr[j]);
-    //         currans=Math.max(currans, currmax*(j-idx+1)+dp[idx+1]);
-    //         ans=Math.max(ans, currans);
-    //     }
-    //     dp[idx]=ans;
-    //    }
+       //idx represents the max no of subarrys starting fom the index idx
+       for(int idx=dp.length-1;idx>=0;idx--){
+        //we keep a track of maxcurrval and maxlength not exceeding k and 
+        // the index not exceeding the arr.length
 
-    //    return dp[0];
+        int ans=0;
+        int currmax=0;
+        // int currans=0;
 
-        int[]dp=new int[arr.length];
-        // for(int i=0;i<dp.length;i++){
-            Arrays.fill(dp,-1);
-        // }
-        return maxsum(arr, 0, k, dp);
+        for(int j=idx;j<idx+k;j++){
+
+            if(j>=arr.length) break;
+
+            currmax= Math.max(currmax, arr[j]);
+            int currans=currmax*(j-idx+1)+dp[j+1];
+            ans=Math.max(ans, currans);
+        }
+
+        dp[idx]=ans;
+       }
+
+       return dp[0];
+
+        // int[]dp=new int[arr.length];
+        // // for(int i=0;i<dp.length;i++){
+        //     Arrays.fill(dp,-1);
+        // // }
+        // return maxsum(arr, 0, k, dp);
     }
     public int maxsum(int[] arr, int s, int k, int[] dp){
         if(s==arr.length){
@@ -38,6 +41,7 @@ class Solution {
         }
         int ans=0;
         int currmaxval=-1;
+
         for(int i=s;i<s+k;i++){
             if(i>=arr.length) break;
 
