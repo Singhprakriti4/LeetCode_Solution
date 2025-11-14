7fdbe280fce681ace93d902cc528910ba92e9782
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-15 01:25:57 +0530

    Time: 29 ms (82.67%) | Memory: 42.8 MB (97.34%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,29 +1,50 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        int[][] dp=new int[s.length()][s.length()];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                if(i>j){
-                    dp[i][j]=0;//invalid
-                }
-                if(i==j){
-                    dp[i][j]=1;//same character
-                }
-            }
-        }
+        int[] dp=new int[s.length()];
+        dp[s.length()-1]=1;
+        int prev=0;//whatever value is being changed currently
 
-        for(int i=dp.length-1;i>=0;i--){
-            for(int j=i+1;j<dp[0].length;j++){
+        for(int i=s.length()-2;i>=0;i--){
+            dp[i]=1;
+            prev=0;
+            for(int j=i+1;j<s.length();j++){
                 if(s.charAt(i)==s.charAt(j)){
-                    dp[i][j]=2+dp[i+1][j-1];
+                    int temp=prev;
+                    prev=dp[j];
+                    dp[j]=temp+2;
                 }
                 else{
-                    dp[i][j]=Math.max(dp[i+1][j],dp[i][j-1]);
+                    prev=dp[j];
+                    dp[j]=Math.max(dp[j-1],dp[j]);
                 }
             }
         }
 
-        return dp[0][s.length()-1];
+        return dp[s.length()-1];
+        // int[][] dp=new int[s.length()][s.length()];
+        // for(int i=0;i<dp.length;i++){
+        //     for(int j=0;j<dp[0].length;j++){
+        //         if(i>j){
+        //             dp[i][j]=0;//invalid
+        //         }
+        //         if(i==j){
+        //             dp[i][j]=1;//same character
+        //         }
+        //     }
+        // }
+
+        // for(int i=dp.length-1;i>=0;i--){
+        //     for(int j=i+1;j<dp[0].length;j++){
+        //         if(s.charAt(i)==s.charAt(j)){
+        //             dp[i][j]=2+dp[i+1][j-1];
+        //         }
+        //         else{
+        //             dp[i][j]=Math.max(dp[i+1][j],dp[i][j-1]);
+        //         }
+        //     }
+        // }
+
+        // return dp[0][s.length()-1];
         // int[][] dp=new int[s.length()][s.length()];
         // for(int i=0;i<dp.length;i++){
         //     Arrays.fill(dp[i], -1);
