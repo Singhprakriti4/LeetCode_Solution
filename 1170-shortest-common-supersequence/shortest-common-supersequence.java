2ddd413d7f9fce478bd9df7a5783a7d9aca592d1
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-15 02:52:43 +0530

    Time: 23 ms (42.26%) | Memory: 53.3 MB (20.56%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -1,38 +1,75 @@
 class Solution {
-   
     public String shortestCommonSupersequence(String str1, String str2) {
+        String common=lcs(str1,str2);
+        // System.out.println(common);
+        int  i=0;
+        int  j=0;
+        int  k=0;
+        StringBuilder sb=new StringBuilder();
 
-        String[] dp=new String[str2.length()+1];
-        
-        for(int j=0;j<dp.length;j++){
-            dp[j]=str2.substring(j,str2.length());
-        }
-         for(int i=str1.length()-1;i>=0;i--){
-            
-            //initialisation of value of j (last idx of each row)
-            String prev=dp[str2.length()];
-            dp[str2.length()]=str1.substring(i,str1.length());
-            //value of dp[i+1][j+1]
-            
-            for(int j=str2.length()-1;j>=0;j--){
-                String temp = dp[j];
-                if(str1.charAt(i)==str2.charAt(j)){
-                    dp[j]=str1.charAt(i)+ prev;
-                }
-                else{
-                    String s1=str1.charAt(i)+dp[j];
-                    String s2=str2.charAt(j)+dp[j+1];
-                    if(s1.length()<s2.length()){
-                        dp[j]=s1;
-                    }
-                    else{
-                        dp[j]=s2;
-                    }
-                }
-                prev=temp;
+        while(k<common.length()){
+            if(str1.charAt(i)==common.charAt(k) && str2.charAt(j)==common.charAt(k)){
+                sb.append(str1.charAt(i));
+                i+=1;
+                j+=1;
+                k+=1;
+            }
+            else if(str1.charAt(i)==common.charAt(k)){
+                sb.append(str2.charAt(j));
+                j+=1;
+            }
+            else if(str2.charAt(j)==common.charAt(k)){
+                sb.append(str1.charAt(i));
+                i+=1;
+            }
+            else{
+                //all 3 are distinct
+                sb.append(str1.charAt(i));
+                sb.append(str2.charAt(j));
+                i+=1;
+                j+=1;
             }
         }
-        
-        return dp[0];
+        if(i<str1.length()){
+            sb.append(str1.substring(i));
+        }
+        if(j<str2.length()){
+            sb.append(str2.substring(j));
+        }
+        return sb.toString();
+    }
+    public String lcs(String s1, String s2 ){
+        int[][] dp=new int[s1.length()+1][s2.length()+1];
+        for(int idx=0;idx<dp.length;idx++){
+            dp[idx][s2.length()]=0;//if we reached the end of string 2
+        }
+        for(int idx=0;idx<dp[0].length;idx++){
+            dp[s1.length()][idx]=0;//if we reached the end of string 1
+        }
+        for(int i=dp.length-2;i>=0;i--){
+            for(int j=dp[0].length-2;j>=0;j--){
+                if(s1.charAt(i)==s2.charAt(j)){
+                    dp[i][j]=1+dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
+        }
+        StringBuilder ans=new StringBuilder();
+        return makelcs(dp,0,0,s1,s2,ans);
+    }
+    public String makelcs(int[][] dp, int r, int c, String s1, String s2,
+    StringBuilder sb){
+        if(dp[r][c]==0){
+            return sb.toString();
+        }
+        if(s1.charAt(r)==s2.charAt(c)){
+            return makelcs(dp,r+1,c+1,s1,s2,sb.append(s1.charAt(r)));
+        }
+        else if(dp[r+1][c]==dp[r][c]){
+            return makelcs(dp,r+1, c, s1, s2, sb);
+        }
+        return makelcs(dp,r,c+1,s1,s2,sb);
     }
 }
