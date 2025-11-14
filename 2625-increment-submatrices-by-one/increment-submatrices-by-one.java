87601055dac19e4a1bc357bb1b0d14900b1f5161
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-15 00:48:15 +0530

    Time: 10 ms (85.09%) | Memory: 77.3 MB (11.84%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
@@ -1,23 +1,44 @@
 class Solution {
     public int[][] rangeAddQueries(int n, int[][] queries) {
+
         int[][] mat=new int[n][n];
+
         for(int i=0;i<queries.length;i++){
             int r1=queries[i][0];
             int c1=queries[i][1];
             int r2=queries[i][2];
             int c2=queries[i][3];
 
-            for(int r=r1;r<=r2;r++){
-                mat[r][c1]+=1;
-                if(c2+1<n){
-                mat[r][c2+1]-=1;
-                }
+           mat[r1][c1]+=1;
+
+           if(r2+1<n){
+            mat[r2+1][c1]-=1;
+           }
+           if(c2+1<n){
+            mat[r1][c2+1]-=1;
+            if(r2+1<n){
+                mat[r2+1][c2+1]+=1;
+            }
+           }
+        }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         System.out.print(mat[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+        //vertical sumup
+        for(int col=0;col<n;col++){
+            for(int row=1;row<n;row++){
+                mat[row][col]=mat[row-1][col]+mat[row][col];
             }
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=1;j<n;j++){
-                mat[i][j]=mat[i][j-1]+mat[i][j];
+        //horizontal sumup
+        for(int row=0;row<n;row++){
+            for(int col=1;col<n;col++){
+                mat[row][col]=mat[row][col-1]+mat[row][col];
             }
         }
 
