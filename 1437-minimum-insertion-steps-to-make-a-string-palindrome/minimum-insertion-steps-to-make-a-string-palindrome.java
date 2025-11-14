4c3b7bf5eb66daeb92edbb00cce6b8fba6257679
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-15 01:43:57 +0530

    Time: 19 ms (86.37%) | Memory: 46.4 MB (23.46%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -4,21 +4,19 @@ class Solution {
         for(int i=0;i<dp.length;i++){
             Arrays.fill(dp[i],-1);
         }
-        return insert(s,0,s.length()-1,dp);
+        return inserts(s,0,s.length()-1,dp);
     }
-    public int insert(String s, int i, int j,int[][] dp){
-        if(i==j){
-            return 0;
-        }
-        if(i>j){
+    public int inserts(String s, int i, int j,int[][] dp){
+        if(i>=j){
             return 0;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
         if(s.charAt(i)==s.charAt(j)){
-            return dp[i][j]=insert(s,i+1,j-1,dp);
+            return dp[i][j]=inserts(s,i+1,j-1,dp);
         }
-        return dp[i][j]=Math.min(insert(s,i+1,j,dp),insert(s,i,j-1,dp))+1;
+        // System.out.println(s.charAt(i)+" "+s.charAt(j));
+        return dp[i][j]=Math.min(1+inserts(s,i+1,j,dp),1+inserts(s,i,j-1,dp));
     }
 }
