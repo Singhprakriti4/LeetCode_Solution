0eb5fc34f3a20411a2d62b4a7cefd0a662365161
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-15 03:33:52 +0530

    Time: 19 ms (64.87%) | Memory: 54.3 MB (19.78%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,30 +1,38 @@
 class Solution {
     public int numDistinct(String s, String t) {
-    int[][] dp=new int[s.length()][t.length()];
-    for(int[] d:dp){
-        Arrays.fill(d,-1);
-    }    
-   
-    return count(s.length()-1,t.length()-1,s,t,dp);
+        int[][] dp=new int[s.length()+1][t.length()+1];
+    
+        for(int i=0;i<dp[0].length;i++){
+            dp[s.length()][i]=0;
+        }
+        for(int i=0;i<dp.length;i++){
+            dp[i][t.length()]=1;
+        }
+        for(int i=dp.length-2;i>=0;i--){
+            for(int j=dp[0].length-2;j>=0;j--){
+                if(s.charAt(i)==t.charAt(j)){
+                    dp[i][j]=dp[i+1][j+1]+dp[i+1][j];
+                }
+                else{
+                    dp[i][j]=dp[i+1][j];
+                }
+            }
+        }
+        return dp[0][0];
+        // return subseq(s,t,0,0);
     }
-    public int count(int i, int j, String s1, String s2,int[][] dp){
-        if(j<0){
-            return 1;
-        }
-        if(i<0){
-            return 0;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1.charAt(i)!=s2.charAt(j)){
-            dp[i][j]=count(i-1,j,s1,s2,dp);
-        }
-        else{
-        dp[i][j]=count(i-1,j,s1,s2,dp)+count(i-1,j-1,s1,s2,dp);
+    // public int subseq(String s, String t, int i, int j){
+    //     //base case
+    //     if(j==t.length()){
+    //         return 1;
+    //     }
+    //     if(i==s.length()){
+    //         return 0;
+    //     }
 
-        }
-        return dp[i][j];
-
-        
-    }
-
+    //     if(s.charAt(i)==t.charAt(j)){
+    //         return subseq(s,t,i+1,j+1)+subseq(s,t,i+1,j);
+    //     }
+    //     return subseq(s,t,i+1,j);
+    // }
 }
