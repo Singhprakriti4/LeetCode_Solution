b716824db3309e498d4030d8968c4deff648fb80
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-15 01:26:21 +0530

    Time: 29 ms (82.67%) | Memory: 42.7 MB (97.56%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -51,20 +51,20 @@ class Solution {
         // }
         // return lps(s,0,s.length()-1,dp);
     }
-    public int lps(String s, int i, int j, int[][] dp){
-        if(i==j){
-            return 1;
-        }
-        if(i>j){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(s.charAt(i)==s.charAt(j)){
-            return dp[i][j]=2+lps(s,i+1,j-1,dp);
-        }
+    // public int lps(String s, int i, int j, int[][] dp){
+    //     if(i==j){
+    //         return 1;
+    //     }
+    //     if(i>j){
+    //         return 0;
+    //     }
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+    //     if(s.charAt(i)==s.charAt(j)){
+    //         return dp[i][j]=2+lps(s,i+1,j-1,dp);
+    //     }
         
-        return dp[i][j]=Math.max(lps(s,i+1,j,dp),lps(s,i,j-1,dp));
-    }
+    //     return dp[i][j]=Math.max(lps(s,i+1,j,dp),lps(s,i,j-1,dp));
+    // }
 }
