ee7f83c1b655f4e49ced649a1bfcef5d3a6cfe76
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-19 03:36:50 +0530

    Time: 9 ms (5.04%) | Memory: 45.8 MB (5.46%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -4,7 +4,7 @@ class Solution {
         return nqueens(n,0,new HashSet<>());
     }
 
-    public int nqueens(int n, int r, Set<String> set){
+    public int nqueens(int n, int r, Set<int[]> set){
         if(r==n){
             return 1;
         }
@@ -16,7 +16,8 @@ class Solution {
             
             if(checkdiagonal(set,r,i,n) ){
                 //queen can be put here
-                String s=r+":"+i;
+                // String s=r+":"+i;
+                int[] s={r,i};
                 set.add(s);
                 ans+=nqueens(n,r+1,set);
                 set.remove(s);
@@ -25,38 +26,10 @@ class Solution {
 
         return ans;
     }
-    public boolean checkdiagonal(Set<String> set, int r, int c, int n){
-        // int testr=r;
-        // int testc=c;
-
-        // while(testr>=0 && testc<n){
-        //     if(set.contains(testr+":"+testc)){
-        //         return false;
-        //     }
-        //     testr-=1;
-        //     testc+=1;
-        // }
-
-        // testr=r;
-        // testc=c;
-        // while(testr>=0 && testc>=0){
-        //     if(set.contains(testr+":"+testc)) return false;
-        //     testr-=1;
-        //     testc-=1;
-        // }
-
-        // testr=r;
-        // testc=c;
-        // while(r>=0){
-        //     if(set.contains(testr+":"+testc)) return false;
-        //     testr--;
-        // }
-
-        // return true;
-        for(String str: set){
-            String[] arr=str.split(":");
-            int row=Integer.parseInt(arr[0]);
-            int col=Integer.parseInt(arr[1]);
+    public boolean checkdiagonal(Set<int[]> set, int r, int c, int n){
+        for(int[] arr: set){
+            int row=arr[0];
+            int col=arr[1];
 
             if(col==c) return false;
             int diff=r-row;
