dafbcfa2db0d934f82019a702e3c803d82d56921
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-19 04:16:28 +0530

    Time: 1 ms (81.79%) | Memory: 42 MB (17.55%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -1,37 +1,32 @@
 class Solution {
-    //diagonal attack is also considerd
-    public int totalNQueens(int n) {
+    public int help(int i,int n,int[] col,int[] topLeft,int[] topRight){
+        if(i==n) return 1;
+        int count=0;
+        for(int j=0;j<n;j++){
+            if((col[j]==0) && (topLeft[i-j+n-1] ==0) && (topRight[i+j]==0)){
+                col[j]=1;
+                topLeft[i-j+n-1]=1;
+                topRight[i+j]=1;
 
-        return nqueens(n,0,new HashSet<>(),new HashSet<>(),new HashSet<>());
-    }
+                count += help(i+1,n,col,topLeft,topRight);
 
-    public int nqueens(int n, int r, Set<Integer> toplevelleft,
-    Set<Integer> toplevelright, Set<Integer> cols){
-        if(r==n){
-            return 1;
-        }
-        //see if we can place the current queen in any of the columns
-        int ans=0;
-
-        for(int i=0;i<n;i++){
-
-            int top1=i-r;
-            int top2=i+r;
-
-            if(!toplevelleft.contains(top1) && !toplevelright.contains(top2)
-            && !cols.contains(i)){
-
-                //queen can be put here
-                toplevelleft.add(top1);
-                toplevelright.add(top2);
-                cols.add(i);
-                ans+=nqueens(n,r+1,toplevelleft,toplevelright,cols);
-                toplevelleft.remove(top1);
-                toplevelright.remove(top2);
-                cols.remove(i);
+                col[j]=0;
+                topLeft[i-j+n-1]=0;
+                topRight[i+j]=0;
             }
         }
+        return count;
+    }
+    public int totalNQueens(int n) {
+        int ans = 0;
 
-        return ans;
+        int[] col = new int[n];
+        Arrays.fill(col,0);
+        int[] topLeft = new int[2*n];
+        Arrays.fill(topLeft,0);
+        int[] topRight = new int[2*n];
+        Arrays.fill(topRight,0);
+        
+        return help(0,n,col,topLeft,topRight);
     }
 }
