6ce9ecd8c0addb0250fe672ddd2783540ccbbb23
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-22 01:13:15 +0530

    Time: 6 ms (53.04%) | Memory: 47.6 MB (24.41%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,31 +10,19 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if((root.val>p.val && root.val<q.val)|| (root.val<p.val && root.val>q.val)){
+        if(root==null){
+            return null;
+        }
+        if(root==p || root==q){
             return root;
         }
-        
+        if((root.val>p.val && root.val<q.val) ||
+         (root.val>q.val && root.val<p.val)){
+            return root;
+        }
         if(root.val>p.val && root.val>q.val){
-           return lowestCommonAncestor(root.left,p,q);
+            return lowestCommonAncestor(root.left, p,q);
         }
-
-        if(root.val<p.val && root.val<q.val){
-            return lowestCommonAncestor(root.right,p,q);
-        }
-
-        if((root.val==p.val && hasNode(root,q))||(root.val==q.val && hasNode(root,p)))
-        return root;
-
-        return null;
-        
-    }
-    public boolean hasNode(TreeNode root, TreeNode target){
-        if(root==null)
-        return false;
-
-        if(root.val==target.val)
-        return true;
-
-        return hasNode(root.left,target)||hasNode(root.right,target);
+        return lowestCommonAncestor(root.right,p,q);
     }
 }
