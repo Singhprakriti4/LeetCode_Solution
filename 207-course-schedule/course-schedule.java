586325fc25145a1f0867f69fa43d330a8f65ac81
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-23 18:40:26 +0530

    Time: 6 ms (74.09%) | Memory: 47 MB (28.13%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,46 +1,43 @@
 class Solution {
-
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        
-        ArrayList<Integer>[] adj=new ArrayList[numCourses];
-        for(int i=0;i<adj.length;i++){
-            ArrayList<Integer> list=new ArrayList<>();
-            adj[i]=list;
+       //making adjecency list+indegreelist
+       int[] indegree=new int[numCourses];
+       List<Integer>[] adj=new ArrayList[numCourses];
+
+       for(int i=0;i<adj.length;i++){
+        adj[i]=new ArrayList<>();
+       }
+
+       for(int i=0;i<prerequisites.length;i++){
+        int a=prerequisites[i][0];
+        int b=prerequisites[i][1];
+
+        adj[b].add(a);
+        indegree[a]+=1;
+       } 
+
+       //starting the topological sort
+       Queue<Integer> q=new LinkedList<>();
+       for(int i=0;i<indegree.length;i++){
+        if(indegree[i]==0){
+            q.add(i);
         }
+       }
 
-        for(int i=0;i<prerequisites.length;i++){
-            int from=prerequisites[i][1];
-            int to=prerequisites[i][0];
+       int taken=0;
 
-            adj[from].add(to);
-        }
-
-        boolean[] visited=new boolean[numCourses];
-        boolean[] currpath=new boolean[numCourses];
-        for(int i=0;i<visited.length;i++){
-            if(!visited[i]){
-                if(dfs(visited,currpath,adj,i,-1)) return false;
+       while(!q.isEmpty()){
+        int c=q.poll();
+        taken+=1;
+        List<Integer> neighbours=adj[c];
+        for(int n: neighbours){
+            indegree[n]-=1;
+            if(indegree[n]==0){
+                q.add(n);
             }
         }
+       }
 
-        return true;
-    }
-    public boolean dfs(boolean[] visited,boolean[] currpath, ArrayList<Integer>[] adj,
-    int node, int parent){
-        visited[node]=true;
-        currpath[node]=true;
-
-        for(int i=0;i<adj[node].size();i++){
-            int vertex=adj[node].get(i);
-            if(!visited[vertex]){
-                if(dfs(visited,currpath,adj,vertex,node)) return true;
-            }
-            else if(currpath[vertex]==true){
-                return true;
-            }
-        }
-
-        currpath[node]=false;
-        return false;
+       return taken==numCourses;
     }
 }
