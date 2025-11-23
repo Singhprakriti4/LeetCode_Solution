9b6c76db4e93a5f5e595fcca8f85ada295b06270
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-24 01:08:47 +0530

    Time: 5 ms (97.83%) | Memory: 46.8 MB (9.62%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,48 +1,28 @@
 class Solution {
-    //dijkastrs
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-       ArrayList<int[]>[] adj=new ArrayList[n];
-       for(int i=0;i<adj.length;i++){
-        adj[i]=new ArrayList<>();
-       }
-
-       for(int i=0;i<flights.length;i++){
-        adj[flights[i][0]].add(new int[]{flights[i][1],flights[i][2]});
-       }
        int[] dist=new int[n];
-       Arrays.fill(dist,Integer.MAX_VALUE);
+       Arrays.fill(dist, Integer.MAX_VALUE);
        dist[src]=0;
 
-       Queue<int[]> q=new LinkedList<>();
-       q.add(new int[]{0,src,0});
+      for(int i=0;i<=k;i++){
+        //relax each edge 
+        int[] temp=dist.clone();
 
-       while(!q.isEmpty()){
+        for(int j=0;j<flights.length;j++){
+            int from=flights[j][0];
+            int to=flights[j][1];
+            int wt=flights[j][2];
 
-        int[] arr=q.poll();
-        int cost=arr[0];
-        int node=arr[1];
-        int stops=arr[2];
+            if(dist[from] !=Integer.MAX_VALUE &&
+            temp[to]>dist[from]+wt){
+                temp[to]=dist[from]+wt;
+            }
 
-        if(stops>k){
-            continue;
         }
+        dist=temp;
+      }
 
-        for(int i=0;i<adj[node].size();i++){
-
-            int vertex=adj[node].get(i)[0];
-            int price=adj[node].get(i)[1];
-
-            if(price+cost<dist[vertex] && stops<=k){
-            dist[vertex]=price+cost;
-            q.add(new int[]{cost+price,vertex,stops+1});
-            
-           }
-        }
-       }
-
-       if(dist[dst]==Integer.MAX_VALUE)return -1;
-
-       return dist[dst];
-
+      if(dist[dst]==Integer.MAX_VALUE) return -1;
+      return dist[dst];
     }
 }
