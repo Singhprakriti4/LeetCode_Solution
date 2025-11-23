8799fa518c9a6db42e72f0e2db5fdc8b33a43de5
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-24 02:19:58 +0530

    Time: 10 ms (98.33%) | Memory: 51.8 MB (22.90%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -1,59 +1,50 @@
-class Pair implements Comparable<Pair>{
-    int node;
-    long dist;
-    Pair(int node, long dist){
-        this.node=node;
-        this.dist=dist;
-    }
-    public int compareTo(Pair that){
-        return Long.compare(this.dist, that.dist);
-    }
-} 
 class Solution {
     public int countPaths(int n, int[][] roads) {
-        final int MOD = 1_000_000_007;
+        ArrayList<int[]>[] adj = new ArrayList[n];
+        for (int i = 0; i < n; i++)
+            adj[i] = new ArrayList<>();
 
-        //making adjecency list
-        ArrayList<ArrayList<Integer>>[] adj=new ArrayList[n];
-        for(int i=0;i<adj.length;i++){
-            adj[i]=new ArrayList<>();
-        }  
-        for(int i=0;i<roads.length;i++){
-            adj[roads[i][0]].add(new ArrayList<>(List.of(roads[i][1],roads[i][2])));
-            adj[roads[i][1]].add(new ArrayList<>(List.of(roads[i][0],roads[i][2])));
+        for (int[] r : roads) {
+            adj[r[0]].add(new int[]{r[1], r[2]});
+            adj[r[1]].add(new int[]{r[0], r[2]});
         }
-        
-        long[] ans=new long[n];
-        int[] ways=new int[n];
-        Arrays.fill(ans,Long.MAX_VALUE);
 
-        PriorityQueue<Pair> q=new PriorityQueue<>();
-        q.add(new Pair(0,0));
-        ans[0]=0;
-        ways[0]=1;
+        PriorityQueue<long[]> pq = new PriorityQueue<>((a,b)->Long.compare(a[1], b[1]));
 
-        while(!q.isEmpty()){
-            Pair p=q.poll();
-            int node=p.node;
-            long dist=p.dist;
-          
-                //neighbours
-                ArrayList<ArrayList<Integer>> neighbours=adj[node];
-                for(ArrayList<Integer> list:neighbours){
-                    int vertex=list.get(0);
-                    int weight=list.get(1);
-                    if(ans[vertex]==weight+dist){
-                        ways[vertex]=(ways[vertex]+ways[node])%MOD;
-                    }
-                    else if(ans[vertex]>weight+dist){
-                        ways[vertex]=ways[node];
-                        ans[vertex]=weight+dist;
-                        q.add(new Pair(vertex,ans[vertex]));
-                    }
+        long[] ways = new long[n];
+        long[] dist = new long[n];
+        Arrays.fill(dist, Long.MAX_VALUE);
+
+        dist[0] = 0;
+        ways[0] = 1;
+
+        pq.add(new long[]{0, 0}); // node, dist
+
+        int MOD = 1_000_000_007;
+
+        while (!pq.isEmpty()) {
+            long[] cur = pq.poll();
+            int node = (int)cur[0];
+            long time = cur[1];
+
+            if (time > dist[node]) continue;
+
+            for (int[] e : adj[node]) {
+                int nei = e[0];
+                long wt = e[1];
+                long newTime = time + wt;
+
+                if (newTime < dist[nei]) {
+                    dist[nei] = newTime;
+                    ways[nei] = ways[node];
+                    pq.add(new long[]{nei, newTime});
                 }
-            
+                else if (newTime == dist[nei]) {
+                    ways[nei] = (ways[nei] + ways[node]) % MOD;
+                }
+            }
         }
-        System.out.println(ans[n-1]);
-        return ways[n-1]%MOD;
+
+        return (int) ways[n-1];
     }
 }
