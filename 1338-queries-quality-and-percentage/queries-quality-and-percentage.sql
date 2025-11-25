fcb8b1a69f97535419ca6c53d40be19a367ff03b
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-11-25 23:52:22 +0530

    Time: 357 ms (39.91%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,26 +1,9 @@
 # Write your MySQL query statement below
-select 
-q.query_name, 
-t3.quality,
-case 
-when  lessthanthree is null then 0
-else 
-round((lessthanthree/count(rating))*100,2) 
-end as poor_query_percentage
-from 
-queries q
-inner join
-(select t1.query_name, t1.quality, t2.lessthanthree
-from
-(select *, 
-round(avg(rating/position),2) as quality
-from queries
-group by query_name) as t1
-left join
-(select *, count(rating) as lessthanthree
-from queries
-where rating<3
-group by query_name) as t2
-on t1.query_name=t2.query_name) as t3
-on q.query_name=t3.query_name
-group by q.query_name;
+select  
+   query_name,
+   round(avg(rating/position),2) as quality,
+   round((sum(case when rating<3 then 1 else 0 end )/count(*))*100, 2)
+   as poor_query_percentage
+   from queries
+   group by query_name;
+
