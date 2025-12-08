1102e323c1442fa6512c2e840b94572b054602b3
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-12-08 20:12:04 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (20.98%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,32 +1,36 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-         if(nums[0]<nums[nums.length-1]){
-            return binary(nums,target,0,nums.length-1);
-         }
-         else{
-            int pivot=-1;
-            for(int i=0;i<nums.length-1;i++){
-                if(nums[i]>nums[i+1]){
-                    pivot=i;
-                }
-            }
-            return (binary(nums,target,0,pivot) || binary(nums,target,pivot+1,nums.length-1));
-         }
-       
-    }
-    boolean binary(int[] nums, int target,int start, int end){
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                return true;
-            }
-            else if(nums[mid]>target){
-                end=mid-1;;
+      int s=0;
+      int e=nums.length-1;
+      while(s<=e){
+        int m=s+(e-s)/2;
+        if(nums[m]==target){
+            return true;
+        }
+        //if we are confused
+        if(nums[s]==nums[m] && nums[m]==nums[e]){
+            s+=1;
+            e-=1;
+        }
+        else if(nums[m]>nums[e]){
+            //left part is sorted
+            if(target>=nums[s] && target<nums[m]){
+                e=m-1;
             }
             else{
-                start=mid+1;
+                s=m+1;
             }
         }
-        return false;
+        else{
+            //right part is sorted
+            if(target>nums[m] && target<=nums[e]){
+                s=m+1;
+            }
+            else{
+                e=m-1;
+            }
+        }
+      }  
+      return false;
     }
 }
