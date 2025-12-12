0aba5b804bb9d7db4094b0e507f043795f197a37
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-12-12 19:04:18 +0530

    Time: 3 ms (91.41%) | Memory: 48.2 MB (22.95%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,68 +1,27 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int l=matrix.length;
-        int n=matrix[0].length;
-        if(target>matrix[l-1][n-1]) return false;
+    public boolean searchMatrix(int[][] matrix, int target) { 
 
-        int r=l-1;
-        int c=n-1;
-        while(r>=0 && c>=0){
-            //for the cirrent roe
-            if(target<matrix[r][0]){
-                //not in currrnt row
-                r-=1;
+        // int r=matrix.length-1;
+        // int c=matrix[0].length-1;
+
+        // while(r>=0 && c>=0){
+        //   if(matrix[r][c]==target) return true;
+        //   if(matrix[r][0]>target) r--;
+        //   if(matrix[0][c]>target) c--;
+        // }
+        // return false;
+
+        int r=0;
+        int c=matrix[0].length-1;
+        while(r<matrix.length && c>=0){
+            if(matrix[r][c]==target){
+                return true;
+            }
+            if(matrix[r][c]>target) {
+                c--;
             }
             else{
-                boolean ans=false;
-                int s=0;
-                int e=c;
-                while(s<=e){
-                    int m=s+(e-s)/2;
-                    int val=matrix[r][m];
-                    if(val==target){
-                        ans=true;
-                        return true;
-                    }
-                    else if(val>target){
-                        e=m-1;
-                    }
-                    else{
-                        s=m+1;
-                    }
-                }
-                if(!ans){
-                    r-=1;
-                    //not in the current row;
-                }
-            }
-
-            if(target<matrix[0][c]){
-                //not in current column;
-                c-=1;
-            }
-            else{
-                int s=0;
-                int e=r;
-                boolean ans=false;
-                
-                while(s<=e){
-                    int m=s+(e-s)/2;
-                    int val=matrix[m][c];
-                    if(val==target){
-                        ans=true;
-                        return true;
-                    }
-                    else if(val>target){
-                        e=m-1;
-                    }
-                    else{
-                        s=m+1;
-                    }
-                }
-                if(!ans){
-                    c-=1;
-                    //not in the current column
-                }
+                r++;
             }
         }
         return false;
