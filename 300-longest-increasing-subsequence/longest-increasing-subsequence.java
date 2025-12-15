164f8e77e617f8a6f1876fe44dbf3648c8b47133
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-12-16 01:14:40 +0530

    Time: 5 ms (90.99%) | Memory: 46 MB (54.08%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,23 +1,36 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-       int[] lis=new int[nums.length];
-       lis[0]=1;
-       for(int i=1;i<lis.length;i++){
-        int max=0;
-        for(int j=i-1;j>=0;j--){
-            if(nums[j]<nums[i]){
-                //a potential candidate
-                max=Math.max(max,lis[j]);
+        List<Integer> list=new ArrayList<>();
+        list.add(nums[0]);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>list.get(list.size()-1)){
+                list.add(nums[i]);
+            }
+            else{
+                //find it's correct place
+                binary(list,nums[i]);
             }
         }
-        lis[i]=1+max;
-       } 
-       int maxlislength=0;
-
-       for(int k: lis){
-        //cuz it is not mandatory that the lis will end at the las index only
-        maxlislength=Math.max(maxlislength, k);
-       }
-       return maxlislength;
+        return list.size();
+    }
+    public void binary(List<Integer> list, int num){
+        //find the first elemnt that is just greater than the currnet num
+        int s=0;
+        int e=list.size()-1;
+        int ans=-1;
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(list.get(m)>num){
+                ans=m;
+                e=m-1;
+            }
+            else{
+                s=m+1;
+            }
+        }
+        if(ans!=-1){
+        if(ans==0 ||  list.get(ans-1)<num)
+        list.set(ans,num);
+        }
     }
 }
