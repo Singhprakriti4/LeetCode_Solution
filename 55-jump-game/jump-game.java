be93884b6d6164e82986e9107a864402d9b6d54a
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-12-16 03:03:59 +0530

    Time: 541 ms (5.02%) | Memory: 47.8 MB (40.45%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,31 +1,50 @@
 class Solution {
     public boolean canJump(int[] nums) {
+
         int[] dp=new int[nums.length];
-        Arrays.fill(dp,-1);
-        //-1:null, 0:false, 1:true
-       return jump(nums,0,dp);     
-    }
-    public boolean jump(int[] nums, int i, int[] dp){
-        if(i>=nums.length){
-            return false;
+        // dp[nums.length]=0;
+        dp[nums.length-1]=1;
+
+        for(int i=dp.length-2;i>=0;i--){
+            //go for jump
+            for(int jump=1;jump<=nums[i];jump++){
+                if(i+jump<nums.length){
+                    if(dp[i+jump]==1){
+                        dp[i]=1;
+                    }
+                }
+                else{
+                    break;
+                }
+            }
         }
-        if(i==nums.length-1){
-            return true;
-        }
-        if(dp[i]!=-1){
-            if(dp[i]==0) return false;
-            return true;
-        }
-        boolean ans=false;
-        for(int jump=1;jump<=nums[i];jump++){
-            ans= ans || jump(nums, i+jump, dp);
-        }
-        if(ans==true){
-            dp[i]=1;
-        }
-        else{
-            dp[i]=0;
-        }
-        return ans;
+        return dp[0]==1;
+    //     int[] dp=new int[nums.length];
+    //     Arrays.fill(dp,-1);
+    //     //-1:null, 0:false, 1:true
+    //    return jump(nums,0,dp);     
+    // }
+    // public boolean jump(int[] nums, int i, int[] dp){
+    //     if(i>=nums.length){
+    //         return false;
+    //     }
+    //     if(i==nums.length-1){
+    //         return true;
+    //     }
+    //     if(dp[i]!=-1){
+    //         if(dp[i]==0) return false;
+    //         return true;
+    //     }
+    //     boolean ans=false;
+    //     for(int jump=1;jump<=nums[i];jump++){
+    //         ans= ans || jump(nums, i+jump, dp);
+    //     }
+    //     if(ans==true){
+    //         dp[i]=1;
+    //     }
+    //     else{
+    //         dp[i]=0;
+    //     }
+    //     return ans;
     }
 }
