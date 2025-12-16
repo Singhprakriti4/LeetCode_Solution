9f950b3b55fa69c353975b0482e20f5add8f3807
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-12-16 17:01:32 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (10.64%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -3,30 +3,22 @@ class Solution {
         if(nums.length==1){
             return nums[0];
         }
-        if(nums.length==2){
-            return Math.max(nums[0],nums[1]);
-        }
-        //make two dps : with taking one and without taking one and choose the most one
-
-        int[] dptrue=new int[nums.length];//go only till length-2
-        int[] dpfalse=new int[nums.length];// start with the second index and go till last
-
-        dptrue[0]=nums[0];//took the first element
-        dptrue[1]=Math.max(nums[0],nums[1]);
+        int[] dp=new int[nums.length];
+        dp[0]=nums[0];//robbed the first house
+        dp[1]=Math.max(nums[0],nums[1]);
+        //cannot rob the last oone
         for(int i=2;i<nums.length-1;i++){
-            dptrue[i]=Math.max(dptrue[i-2]+nums[i], dptrue[i-1]);
+            dp[i]=Math.max(dp[i-1], nums[i]+dp[i-2]);
         }
+        int ans=dp[nums.length-2];
 
-        int ans1=dptrue[nums.length-2];
-
-        dpfalse[0]=0;//not considering the first one
-        dpfalse[1]=nums[1];
+        dp[0]=0;//did not rob the first house
+        dp[1]=nums[1];
+        //can rob the last one
         for(int i=2;i<nums.length;i++){
-            dpfalse[i]=Math.max(dpfalse[i-2]+nums[i], dpfalse[i-1]);
+            dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2]);
         }
-
-        int ans2=dpfalse[nums.length-1];
-
-        return Math.max(ans1,ans2);
+        ans=Math.max(ans, dp[nums.length-1]);
+        return ans;
     }
 }
