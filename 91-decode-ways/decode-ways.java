66302dfc84263c5f30ad99982e3e2f1500de14b4
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-12-16 12:24:23 +0530

    Time: 2 ms (12.85%) | Memory: 43.4 MB (16.07%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,31 +1,45 @@
 class Solution {
     public int numDecodings(String s) {
-    //    if(s.charAt(0)=='0'){
-    //     return 0;
-    //    } 
-       int[] dp=new int[s.length()];
-       Arrays.fill(dp,-1);
-       return decode(s,0,dp);
-    }
-    public int decode(String  s, int i, int[] dp){
-        if(i==s.length()) {
-            return 1;
+        int[] dp=new int[s.length()+1];
+        dp[s.length()]=1;//initialisation
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)=='0'){
+                dp[i]=0;
+            }
+            else{
+                dp[i]=dp[i+1];
+                if(i+1<s.length()){
+                    int curr=Integer.parseInt(s.substring(i,i+2));
+                    if(curr>=1 && curr<=26){
+                        dp[i]+=dp[i+2];
+                    }
+                }
+            }
         }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        if(s.charAt(i)=='0'){
-            return 0;
-        }
-        // if(dp[i])
-        int ans=0;
-        ans=decode(s, i+1, dp);
-        if(i+1<s.length()){
-        int next=Integer.parseInt(s.substring(i, i+2));
-        if(next<=26 && next>=1){
-            ans+=decode(s, i+2, dp);
-        }
-        }
-        return dp[i]=ans;
+        return dp[0];
+    //    int[] dp=new int[s.length()];
+    //    Arrays.fill(dp,-1);
+    //    return decode(s,0,dp);
+    // }
+    // public int decode(String  s, int i, int[] dp){
+    //     if(i==s.length()) {
+    //         return 1;
+    //     }
+    //     if(dp[i]!=-1){
+    //         return dp[i];
+    //     }
+    //     if(s.charAt(i)=='0'){
+    //         return 0;
+    //     }
+    //     // if(dp[i])
+    //     int ans=0;
+    //     ans=decode(s, i+1, dp);
+    //     if(i+1<s.length()){
+    //     int next=Integer.parseInt(s.substring(i, i+2));
+    //     if(next<=26 && next>=1){
+    //         ans+=decode(s, i+2, dp);
+    //     }
+    //     }
+    //     return dp[i]=ans;
     }
 }
