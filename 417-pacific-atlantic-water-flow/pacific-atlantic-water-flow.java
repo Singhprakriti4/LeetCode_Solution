dbcd01f1c4b42f810fd57534ea64c5ceedef89fe
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-12-18 00:22:44 +0530

    Time: 10 ms (33.87%) | Memory: 48.2 MB (6.07%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,101 +1,81 @@
 class Solution {
-    
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-       boolean[][] pacific=new boolean[heights.length][heights[0].length]; 
-       boolean[][] atlantic=new boolean[heights.length][heights[0].length]; 
-       boolean[][] ans=new boolean[heights.length][heights[0].length];
-       for(int i=0;i<heights.length;i++){
-        for(int j=0;j<heights[0].length;j++){
-            if(i==0 || j==0){
-                pacific[i][j]=true;
-            }
-            if(i==heights.length-1 || j==heights[0].length-1){
-                atlantic[i][j]=true;
-            }
-            // System.out.print(pacific[i][j]+" ");
-        }
-        // System.out.println();
-       }
-    //    boolean[][] notans=new boolean[heights.length][heights[0].length];
-
-
-       for(int i=0;i<heights.length;i++){
-        for(int j=0;j<heights[0].length;j++){
-            // System.out.println(i+" "+j+" ");
-        bfs(pacific,atlantic,ans,i,j,heights);
-        }
-       }
-       
-       List<List<Integer>> outer=new ArrayList<>();
-       for(int i=0;i<ans.length;i++){
-        for(int j=0;j<ans[0].length;j++){
-            // System.out.print(ans[i][j]+" ");
-            if(ans[i][j]){
-                List<Integer> inner=new ArrayList<>();
-                inner.add(i);
-                inner.add(j);
-                outer.add(inner);
-            }
-        }
-        // System.out.println();
-       }
-       return outer;
-    }
-    public void bfs(boolean[][] p, boolean[][] a, boolean[][] ans,
-     int i, int j,int[][] heights){
-
-        if(ans[i][j]==true ) return;
-
-        if(p[i][j] && a[i][j]){
-            ans[i][j]=true;
-            return;
-        }
-
-        boolean pac=false;
-        boolean atl=false;
-
-        Queue<int[]> q=new LinkedList<>();
         boolean[][] visited=new boolean[heights.length][heights[0].length];
-        q.add(new int[]{i,j});
-        visited[i][j]=true;
-        if(p[i][j]) pac=true;
-        if(a[i][j]) atl=true;
+        Queue<int[]> q=new LinkedList<>();
 
+        for(int i=0;i<heights.length;i++){
+            q.add(new int[]{i,0});
+            visited[i][0]=true;
+        }
+        for(int j=1;j<heights[0].length;j++){
+            q.add(new int[]{0,j});
+            visited[0][j]=true;
+        }
         while(!q.isEmpty()){
             int[] arr=q.poll();
             int r=arr[0];
             int c=arr[1];
-
             int[] nr={0,0,-1,1};
-            int[] nc={-1,1,0,0};
-
-            for(int k=0;k<4;k++){
-                int row=r+nr[k];
-                int col=c+nc[k];
-
-                if(row>=0 && col>=0 && row<heights.length &&
-                col<heights[0].length && !visited[row][col]
-                && heights[row][col]<=heights[r][c]){
-
-                    if((p[row][col] && a[row][col]) || ans[row][col]
-                    || (pac && atl)){
-                        // System.out.println("hqy");
-                        ans[i][j]=true;
-                        return;
-                    }
-                    else {
-                        if(p[row][col]) pac=true;
-                        if(a[row][col]) atl=true;
-                        visited[row][col]=true;
-                        q.add(new int[]{row,col});
-                    }
+            int[] nc={1,-1,0,0};
+            for(int i=0;i<4;i++){
+                int row=nr[i]+r;
+                int col=nc[i]+c;
+                if(row>=0 && row<heights.length && 
+                col>=0 && col<heights[0].length && heights[row][col]>=heights[r][c]
+                && !visited[row][col]){
+                    visited[row][col]=true;
+                    q.add(new int[]{row,col});
                 }
             }
         }
-        if(pac && atl){
-                ans[i][j]=true;
-                return;
+
+        q=new LinkedList<>();
+        boolean[][] visited2=new boolean[heights.length][heights[0].length];
+        List<List<Integer>> outer=new ArrayList<>();
+
+        for(int i=0;i<heights.length;i++){
+            q.add(new int[]{i,heights[0].length-1});
+            visited2[i][heights[0].length-1]=true;
+            // if(visited[i][heights[0].length-1]){
+            //     List<Integer> inner=new ArrayList<>();
+            //     inner.add(i);
+            //     inner.add(heights[0].length-1);
+            //     outer.add(inner);
+            // }
+        }
+        for(int i=heights[0].length-2;i>=0;i--){
+            q.add(new int[]{heights.length-1, i});
+            visited2[heights.length-1][i]=true;
+            // if(visited[heights.length-1][i]){
+            //     List<Integer> inner=new ArrayList<>();
+            //     inner.add(heights.length-1);
+            //     inner.add(i);
+            //     outer.add(inner);
+            // }
+        }
+        while(!q.isEmpty()){
+            int[] arr=q.poll();
+            int r=arr[0];
+            int c=arr[1];
+            if(visited[r][c]){
+                List<Integer> inner=new ArrayList<>();
+                inner.add(r);
+                inner.add(c);
+                outer.add(inner);
             }
-        return;
+            int[] nr={0,0,-1,1};
+            int[] nc={1,-1,0,0};
+            for(int i=0;i<4;i++){
+                int row=nr[i]+r;
+                int col=nc[i]+c;
+                if(row>=0 && row<heights.length && 
+                col>=0 && col<heights[0].length && heights[row][col]>=heights[r][c]
+                && !visited2[row][col]){
+                    visited2[row][col]=true;
+                    q.add(new int[]{row,col});
+                }
+            }
+        }
+        return outer;
     }
 }
