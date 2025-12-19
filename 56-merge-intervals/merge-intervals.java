69a0d7c7f08decb282ede72307b2358c2c505ac0
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-12-19 18:49:40 +0530

    Time: 15 ms (5.12%) | Memory: 49.2 MB (25.82%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+      Arrays.sort(intervals, (a,b)-> Integer.compare(a[0],b[0]));
+
+      List<List<Integer>> list=new ArrayList<>();
+      List<Integer> l=new ArrayList<>();
+      l.add(intervals[0][0]);
+      l.add(intervals[0][1]);
+      list.add(l);
+
+      for(int i=1;i<intervals.length;i++){
+        int currstart=intervals[i][0];
+        int currend=intervals[i][1];
+        int prevstart=list.get(list.size()-1).get(0);
+        int prevend=list.get(list.size()-1).get(1);
+        List<Integer> currlist=new ArrayList<>();
+
+        if(currstart<=prevend){
+            //need to be merged
+            list.remove(list.size()-1);
+            currlist.add(prevstart);
+            currlist.add(Math.max(currend,prevend));
+        }
+        else{
+            //add the new one directly
+            currlist.add(currstart);
+            currlist.add(currend);
+        }
+        list.add(currlist);
+      }
+
+      int[][] mergedintervals=new int[list.size()][2];
+      for(int i=0;i<list.size();i++){
+        int start=list.get(i).get(0);
+        int end=list.get(i).get(1);
+
+        mergedintervals[i][0]=start;
+        mergedintervals[i][1]=end;
+      }
+
+      return mergedintervals;
+    }
+}
