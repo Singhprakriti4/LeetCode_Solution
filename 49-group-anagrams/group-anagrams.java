5c664e3d136cba296b76c1e8e0be2a2c882ab18a
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-12-22 01:20:11 +0530

    Time: 12 ms (32.10%) | Memory: 49.7 MB (48.25%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -2,44 +2,28 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
         List<List<String>> outer=new ArrayList<>();
-
+         HashMap<List<Character>, List<String>> map=new HashMap<>();
         for(int i=0;i<strs.length;i++){
-            String s=strs[i];
-            if(s.equals("-1")){
-                continue;
+            // if(strs[i].equals("-1")) continue;
+            List<Character> l=new ArrayList<>();
+
+            for(int j=0;j<strs[i].length();j++){
+                l.add(strs[i].charAt(j));//add krdo isko bhi
+        }
+            Collections.sort(l);
+
+            if(map.containsKey(l)){
+                map.get(l).add(strs[i]);
             }
             else{
-                int[] num=new int[26];
-                for(int j=0;j<s.length();j++){
-                    num[(s.charAt(j)-'a')]+=1;
-                }
-                List<String> inner=new ArrayList<>();
-                inner.add(s);
-                for(int j=i+1;j<strs.length;j++){
-                    if(strs[j].equals("-1")) continue;
-                    if(match(s,strs[j],num)){
-                        inner.add(strs[j]);
-                        strs[j]="-1";
-                    }
-                }
-                outer.add(inner);
+                map.put(l, new ArrayList<>());
+                map.get(l).add(strs[i]);
             }
+
+        }
+        for(Map.Entry<List<Character>, List<String>> e: map.entrySet()){
+            outer.add(e.getValue());
         }
         return outer;
-    }
-    public boolean match(String s, String t, int[] nums){
-        if(s.length()!=t.length()){
-            return false;
-        }
-        int[] num=nums.clone();
-        for(int i=0;i<t.length();i++){
-            if(num[(t.charAt(i)-'a')]<=0){
-                return false;
-            }
-            else{
-                num[(t.charAt(i)-'a')]-=1;
-            }
-        }
-        return true;
     }
 }
