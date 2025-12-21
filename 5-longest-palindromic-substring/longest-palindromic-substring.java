076cac83963c255978271a031e0c4736fc1a2a13
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-12-21 21:53:23 +0530

    Time: 160 ms (24.83%) | Memory: 49.1 MB (5.35%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,37 +1,43 @@
 class Solution {
-    String s;
-    Boolean[][] dp;
-    String res = "";
-
     public String longestPalindrome(String s) {
-        this.s = s;
-        int n = s.length();
-        dp = new Boolean[n][n];
-        
-        // check all possible substrings
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (isPalindrome(i, j)) {
-                    if (j - i + 1 > res.length()) {
-                        res = s.substring(i, j + 1);
+        int len=0;
+        String ans="";
+
+        boolean[][] dp=new boolean[s.length()][s.length()];
+        dp[s.length()-1][s.length()-1]=true;
+        len=1;
+        ans=s.substring(s.length()-1);
+
+        for(int i=s.length()-2;i>=0;i--){
+            for(int j=i;j<s.length();j++){
+                if(i==j){
+                    dp[i][j]=true;
+                }
+                else if(s.charAt(i)==s.charAt(j)){
+                    if(i==1 && j==2){
+                    System.out.println("hi");
+                }
+                    int pi=i+1;
+                    int pj=j-1;
+                    // if(pi>pj)
+                    if(pi>pj || (pi<=pj && dp[pi][pj]==true)){
+                        dp[i][j]=true;
+                    }
+                }
+                
+                // if(i==1 && j==2){
+                //     System.out.println(dp[i][j]);
+                // }
+                
+                int curr=j-i+1;
+                if(dp[i][j]){
+                    if(curr>len){
+                        len=curr;
+                        ans=s.substring(i,j+1);
                     }
                 }
             }
         }
-        return res;
-    }
-
-    private boolean isPalindrome(int i, int j) {
-        // base case
-        if (i >= j) return true;
-
-        // memoized result
-        if (dp[i][j] != null) return dp[i][j];
-
-        // recursive check
-        if (s.charAt(i) != s.charAt(j)) 
-            return dp[i][j] = false;
-
-        return dp[i][j] = isPalindrome(i + 1, j - 1);
+        return ans;
     }
 }
