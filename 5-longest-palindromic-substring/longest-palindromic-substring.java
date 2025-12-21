ec28b1f9e95620769ec06c190fe75990c402f0b7
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2025-12-21 22:42:26 +0530

    Time: 15 ms (67.45%) | Memory: 46.3 MB (37.40%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,42 +1,52 @@
 class Solution {
-    public String longestPalindrome(String s) {
+    public String longestPalindrome(String str) {
+
         int len=0;
         String ans="";
+        for(int i=0;i<str.length();i++){
+            int curr=expand(i,i,str);
+            // System.out.println(curr);
+            // if(i==1)
+            // System.out.println(curr);
+            if(len<curr){
+                len=curr;
+                int s=i-(curr/2);
+                int e=s+curr;
+                //  if(i==1){
+                // System.out.println(s);
+                // System.out.println(e);
+                //  }
+                ans=str.substring(s,e);
+                // if(i==1)
+                // System.out.println(ans);
+            }
+            curr=expand(i,i+1,str);
+            if(curr>len){
 
-        boolean[][] dp=new boolean[s.length()][s.length()];
-        dp[s.length()-1][s.length()-1]=true;
-        len=1;
-        ans=s.substring(s.length()-1);
-
-        for(int i=s.length()-2;i>=0;i--){
-            for(int j=i;j<s.length();j++){
+                len=curr;
+                int s=i-(curr/2)+1;
+                int e=s+curr;
+                ans=str.substring(s,e);
+            }
+        }
+        return ans;
+    }
+    public int expand(int i, int j, String s){
+        int ans=0;
+        while(i>=0 && j<s.length()){
+            if(s.charAt(i)==s.charAt(j)){
                 if(i==j){
-                    dp[i][j]=true;
+                    ans+=1;
                 }
-                else if(s.charAt(i)==s.charAt(j)){
-                    if(i==1 && j==2){
-                    System.out.println("hi");
-                }
-                    int pi=i+1;
-                    int pj=j-1;
-                    // if(pi>pj)
-                    if(pi>pj || (pi<=pj && dp[pi][pj]==true)){
-                        dp[i][j]=true;
-                    }
-                }
-                
-                // if(i==1 && j==2){
-                //     System.out.println(dp[i][j]);
-                // }
-                
-                int curr=j-i+1;
-                if(dp[i][j]){
-                    if(curr>len){
-                        len=curr;
-                        ans=s.substring(i,j+1);
-                    }
+                else{
+                    ans+=2;
                 }
             }
+            else{
+                break;
+            }
+            i--;
+            j++;
         }
         return ans;
     }
