ac3626706e59213e359b39f9d444653c6102616a
Singhprakriti4 <123862511+Singhprakriti4@users.noreply.github.com>
2026-01-07 00:34:58 +0530

    Time: 48 ms (46.04%) | Memory: 130.9 MB (73.24%) - LeetSync

---
+++ 146-lru-cache/lru-cache.java
@@ -0,0 +1,27 @@
+class LRUCache {
+
+    int cap;
+    LinkedHashMap<Integer, Integer> cache;
+
+    public LRUCache(int capacity) {
+        cap = capacity;
+
+        cache = new LinkedHashMap<>(capacity, 0.75f, true);
+        // true => access-order mode (moves accessed keys to end)
+    }
+
+    public int get(int key) {
+            return cache.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        cache.put(key, value);
+
+        if (cache.size() > cap) {
+            // remove least-recently-used (first entry)
+            Iterator<Integer> it = cache.keySet().iterator();
+            it.next();
+            it.remove();
+        }
+    }
+}
