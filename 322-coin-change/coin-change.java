452961c347c1326e70bcdc9ab4a5a288aba445ae
Prakriti Singh <123862511+Singhprakriti4@users.noreply.github.com>
2026-02-21 18:30:43 +0530

    Time: 70 ms (5.31%) | Memory: 47.7 MB (8.43%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,72 +1,46 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[][] dp=new int[amount+1][coins.length+1];
-        for(int i=0;i<dp.length;i++){
-            dp[i][0]=-1;//if we donot have any coin
-        }
-        for(int i=0;i<dp[0].length;i++){
-            dp[0][i]=0;//amount is zero
+        int[][] dp=new int[amount+1][coins.length];
+        for(int[] a: dp){
+            Arrays.fill(a,-1);
         }
         
-        for(int row=1;row<amount+1;row++){
-            for(int col=1;col<=coins.length;col++){
-               int take=-1;
-               int leave=-1;
-               if(row-coins[col-1]>=0){
-                take=dp[row-coins[col-1]][col];
-               }
-               leave=dp[row][col-1];
-               if(take==-1 && leave==-1){
-                dp[row][col]=-1;
-               }
-               else if(take==-1){
-                dp[row][col]=leave;
-               }
-               else if(leave==-1){
-                dp[row][col]=1+take;
-               }
-               else{
-                dp[row][col]=Math.min(1+take, leave);
-               }
+        int res=ans(coins, amount, 0, dp);
+        if(res==Integer.MAX_VALUE){
+            return -1;
+        }
+        return res;
+    }
+    public int ans(int[] coins, int amount, int ptr,
+    int[][] dp){
+        if(amount==0){
+            return 0;
+        }
+        if(ptr==coins.length){
+            return Integer.MAX_VALUE;
+        }
+        //take or leave it
+        if(amount<0){
+            return Integer.MAX_VALUE;
+        }
+        if(dp[amount][ptr]!=-1){
+            return dp[amount][ptr];
+        }
+        int a=Integer.MAX_VALUE;
+        int b=Integer.MAX_VALUE;
+
+        if(amount-coins[ptr]>=0){
+            a=ans(coins, amount-coins[ptr], ptr, dp);
+            if(a!=Integer.MAX_VALUE){
+                a+=1;
+            }
+            b=ans(coins, amount-coins[ptr], ptr+1, dp);
+            if(b!=Integer.MAX_VALUE){
+                b+=1;
             }
         }
-
-        return dp[amount][coins.length];
-
-        // for(int i=0;i<dp.length;i++){
-        //     Arrays.fill(dp[i],-2);
-        // }
-        // return noofcoins(coins, amount, coins.length-1,dp);
-    }
-    public int noofcoins(int[] coins, int amt, int idx, int[][] dp){
-        if(amt==0){
-            return 0;//number of coins 
-        }
-        if(idx==-1 || amt<0){
-            return -1;//cannot be taken
-        }
-        if(dp[amt][idx]!=-2){
-            return dp[amt][idx];
-        }
-        int takecoin=noofcoins(coins, amt-coins[idx], idx,dp);
-        int leavecoin=noofcoins(coins, amt, idx-1, dp);
-
-        if(takecoin==-1 && leavecoin==-1){
-            //cannot be taken in any case
-            return  dp[amt][idx]=-1;
-        }
-        else if(takecoin==-1){
-            //leavecoin func is feasible
-            return dp[amt][idx]= leavecoin;
-        }
-        else if(leavecoin==-1){
-            //takecoin is feasible
-            return dp[amt][idx]= 1+takecoin;
-        }
+        int c=ans(coins, amount, ptr+1, dp);
         
-
-        return dp[amt][idx]= Math.min(1+takecoin,leavecoin);
-
+        return dp[amount][ptr]=Math.min(a,Math.min(b,c));
     }
-   
 }
