6d09bcabb1f0b038ffdd01e20b77639a8ebe8206
Prakriti Singh <123862511+Singhprakriti4@users.noreply.github.com>
2026-03-17 18:05:22 +0530

    Time: 1 ms (31.45%) | Memory: 46.5 MB (93.43%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -14,40 +14,23 @@
  * }
  */
 class Solution {
-    int pathsum=Integer.MIN_VALUE;
+    int ans=Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        if(root==null){
-            return 0;
-        }
-        height(root);
-        return pathsum;
+        sum(root);
+        return ans;
     }
-    public int height(TreeNode root){
+    public int sum(TreeNode root){
         if(root==null){
             return 0;
         }
-        int leftpath=height(root.left);
-        int rightpath=height(root.right);
+        int left=sum(root.left);
+        int right=sum(root.right);
 
-        // System.out.println(leftpath);
-        // System.out.println(rightpath);
+        ans=Math.max(ans, Math.max(left+right+root.val, Math.max(left+root.val, 
+        Math.max(right+root.val, root.val))));
 
-        //three cases
-        
-        //case 1
-       int opt1=root.val+leftpath;
-       int opt2=root.val+rightpath;
-       int opt3=root.val+leftpath+rightpath;
-       int opt4=root.val;
-    //    System.out.println("root : "+root.val+"  "+opt1+"  "+opt2+"  "+opt3);
+        return Math.max(left+root.val, 
+        Math.max(right+root.val, root.val));
 
-        int currmax=Math.max(opt1,Math.max(Math.max(opt2,opt3),opt4));
-        pathsum=Math.max(pathsum, currmax);
-
-        //pass the maximum of option 1 and 2
-        //cuz root can either be a path node or the root node
-        //but we can only pass above the path condition of root node
-
-        return Math.max(Math.max(leftpath, rightpath)+root.val, opt4);
     }
 }
