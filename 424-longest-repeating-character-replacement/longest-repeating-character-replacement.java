5a2d6592dbd8f8d562295d582b3b40812a535b5b
Prakriti Singh <123862511+Singhprakriti4@users.noreply.github.com>
2026-03-31 15:46:57 +0530

    Time: 68 ms (5.42%) | Memory: 47.2 MB (14.65%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,51 +1,34 @@
 class Solution {
-    public int characterReplacement(String s, int k) {
-       
-       //for the frequency of each character in a given window
-       HashMap<Character, Integer> map=new HashMap<>();
+    public int characterReplacement(String str, int k) {
 
-       int i=0;
-       int j=0;
-       
-       int len=0;
+        Map<Character, Integer> map=new HashMap<>();
+        int s=0;
+        int e=0;
+        int len=0;
+        int maxlen=0;
 
-      while(j<s.length()){
-        //putting the frequency of the new cahracter so found
-        map.put(s.charAt(j),map.getOrDefault(s.charAt(j),0)+1);
-
-        //see if this is a valid block
-        boolean validblock=false;
-
-        for(Map.Entry<Character,Integer> e: map.entrySet()){
-            //go through each chaarcter frequency in the curr window and see it this window is valid
-            char c=e.getKey();
-            int freq=e.getValue();
-            int currwindowsize=j-i+1;
-
-            //if the frequency of the character+k is >= current window size
-            if(freq+k>=currwindowsize){
-                //possible answer found
-                validblock=true;
-                len=Math.max(len,currwindowsize);
-
-                break;//since no need to check furthur
+        while(s<str.length() && e<str.length()){
+            char curr=str.charAt(e);
+            map.put(curr, map.getOrDefault(curr,0)+1);
+            boolean feasible=false;
+            for(Map.Entry<Character, Integer> entry: map.entrySet()){
+                if(entry.getValue()+k>=e-s+1){
+                    feasible=true;
+                    break;
+                }
+            }
+            if(feasible){
+                e++;
+                len++;
+                maxlen=Math.max(maxlen, len);
+            }
+            else{
+                map.put(curr, map.get(curr)-1);
+                map.put(str.charAt(s), map.get(str.charAt(s))-1);
+                len--;
+                s++;
             }
         }
-
-        if(validblock){
-
-            //try increasing the length for longer possible answer
-            j++;
-
-        }
-        else{
-
-            //decrease the freq of ith character as we will not see any more answer starting from i
-            map.put(s.charAt(i),map.get(s.charAt(i))-1);
-            map.put(s.charAt(j),map.get(s.charAt(j))-1);//cuz this will be automatically added in the next iteration
-            i++;
-        }
-      }
-      return len;
+        return maxlen;
     }
 }
